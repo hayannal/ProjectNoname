15c3bd9f308261ef95e6c39bc9996d63013667dd
hayannal2009 <hayannal2009@gmail.com>
2019-06-09 04:18:25 +0900

    커스텀 에셋 유틸 먼저 추가

---
+++ Assets/Framework/CustomAsset/AnimationCurveAsset.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "NewAnimationCurve", menuName = "CustomAsset/Create Animation Curve", order = 1100)]
+public class AnimationCurveAsset : ScriptableObject
+{
+	public AnimationCurve curve;
+}

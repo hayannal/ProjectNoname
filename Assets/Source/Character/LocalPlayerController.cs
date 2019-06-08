46672bfab784ee801068b9197d148086f3a8eed7
hayannal2009 <hayannal2009@gmail.com>
2019-06-09 04:20:49 +0900

    로컬 플레이어 컨트롤러 추가하고 씬에다가 적용해본다.

---
+++ Assets/Source/Character/LocalPlayerController.cs
@@ -0,0 +1,39 @@
+﻿using ECM.Common;
+using ECM.Controllers;
+using UnityEngine;
+
+public sealed class LocalPlayerController : BaseCharacterController
+{
+	#region EDITOR EXPOSED FIELDS
+
+	[Header("CUSTOM CONTROLLER")]
+	[Tooltip("The character's follow camera.")]
+	public Transform playerCamera;
+
+	#endregion
+
+	#region METHODS
+
+	/// <summary>
+	/// Overrides 'BaseCharacterController' HandleInput,
+	/// to perform custom controller input.
+	/// </summary>
+
+	protected override void HandleInput()
+	{
+		// Handle your custom input here...
+
+		moveDirection = new Vector3
+		{
+			x = UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw("Horizontal"),
+			y = 0.0f,
+			z = UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw("Vertical")
+		};
+
+		// Transform moveDirection vector to be relative to camera view direction
+
+		moveDirection = moveDirection.relativeTo(playerCamera);
+	}
+
+	#endregion
+}

74c80cbb5837c7fcfbfb0f606ef58cfe3a52073e
hayannal2009 <hayannal2009@gmail.com>
2019-06-19 16:32:35 +0900

    이펙트 시그널에 본 어태치 기능 추가

--- Assets/Source/MecanimEvent/MeClass/MeEffect.cs
+++ Assets/Source/MecanimEvent/MeClass/MeEffect.cs
@@ -11,20 +11,35 @@ public class MeEffect : MecanimEventBase {
 	public GameObject effectData;
 	public Vector3 offset;
 	public Vector3 direction = Vector3.forward;
-	
+	public string parentName;
+
 #if UNITY_EDITOR
 	override public void OnGUI_PropertyWindow()
 	{
 		effectData = (GameObject)EditorGUILayout.ObjectField("Object :", effectData, typeof(GameObject), false);
 		offset = EditorGUILayout.Vector3Field("Offset :", offset);
 		direction = EditorGUILayout.Vector3Field("Direction :", direction);
+		parentName = EditorGUILayout.TextField("Parent Transform Name :", parentName);
 	}
 #endif
 
+	DummyFinder _dummyFinder = null;
 	override public void OnSignal(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
-		//Vector3 result = offset * animator.transform.localScale.x;
-		Vector3 rotation = animator.transform.TransformDirection(direction);
-		Instantiate(effectData, animator.transform.TransformPoint(offset), Quaternion.LookRotation(rotation));
+		if (string.IsNullOrEmpty(parentName))
+		{
+			//Vector3 result = offset * animator.transform.localScale.x;
+			Vector3 rotation = animator.transform.TransformDirection(direction);
+			Instantiate(effectData, animator.transform.TransformPoint(offset), Quaternion.LookRotation(rotation));
+		}
+		else
+		{
+			if (_dummyFinder == null) _dummyFinder = animator.GetComponent<DummyFinder>();
+			if (_dummyFinder == null) _dummyFinder = animator.gameObject.AddComponent<DummyFinder>();
+
+			Transform attachTransform = _dummyFinder.FindTransform(parentName);
+			if (attachTransform != null)
+				Instantiate(effectData, attachTransform.position, attachTransform.rotation, attachTransform);
+		}
 	}
 }

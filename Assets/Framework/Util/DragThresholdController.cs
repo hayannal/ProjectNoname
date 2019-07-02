93e6617e164d588d2f1df84dec96bc41a75a6e1f
hayannal2009 <hayannal2009@gmail.com>
2019-07-02 14:20:11 +0900

    스크롤뷰 안에있는 버튼이 안눌리는 거 때문에 썼던 유틸 함수.근데 이거쓰면 스크린 조이스틱도 영향받아서 UI뜰때만 켜야할듯 싶다.
    우선 추가만 해둔다.

---
+++ Assets/Framework/Util/DragThresholdController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragThresholdController : MonoBehaviour {
+
+	private const float inchToCm = 2.54f;
+
+	[SerializeField]
+	private EventSystem eventSystem = null;
+
+	[SerializeField]
+	private float dragThresholdCM = 0.5f;
+	//For drag Threshold
+
+	private void SetDragThreshold()
+	{
+		if (eventSystem != null)
+		{
+			eventSystem.pixelDragThreshold = (int)(dragThresholdCM * Screen.dpi / inchToCm);
+		}
+	}
+
+
+	void Awake()
+	{
+		SetDragThreshold();
+	}
+}

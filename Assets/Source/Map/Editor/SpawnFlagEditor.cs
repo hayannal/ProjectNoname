2f429c0482dbfb8bded17d3edaa070b8f20a5a40
hayannal2009 <hayannal2009@gmail.com>
2019-07-06 17:49:01 +0900

    스폰플래그 스크립트 추가. 씬에 배치해두었던 플래그 프리팹 삭제

---
+++ Assets/Source/Map/Editor/SpawnFlagEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpawnFlag))]
+public class SpawnFlagEditor : Editor
+{
+	SerializedProperty playerSpawnTransformProperty;
+
+	void OnEnable()
+	{
+		playerSpawnTransformProperty = serializedObject.FindProperty("playerSpawnTransform");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		if (GUILayout.Button("Save Spawn Flag Data"))
+		{
+			SpawnFlag targetComponent = (SpawnFlag)target;
+			GameObject prefab = PrefabUtility.GetCorrespondingObjectFromSource<GameObject>(targetComponent.gameObject);
+			if (prefab == null)
+			{
+				EditorUtility.DisplayDialog("Error", "Can't find original Prefab", "Ok");
+				return;
+			}
+
+			string assetPath = AssetDatabase.GetAssetPath(prefab);
+			if (string.IsNullOrEmpty(assetPath))
+			{
+				EditorUtility.DisplayDialog("Error", "Path not found", "Ok");
+				return;
+			}
+
+			SpawnFlag spawnFlagPrefabComponent = prefab.GetComponent<SpawnFlag>();
+			if (spawnFlagPrefabComponent._listSpawnInfo == null)
+				spawnFlagPrefabComponent._listSpawnInfo = new List<SpawnFlag.SpawnInfo>();
+			spawnFlagPrefabComponent._listSpawnInfo.Clear();
+
+			int childCount = targetComponent.cachedTransform.childCount;
+			for (int i = 0; i < childCount; ++i)
+			{
+				Transform childTransform = targetComponent.cachedTransform.GetChild(i);
+				if (childTransform == targetComponent.playerSpawnTransform)
+					continue;
+
+				GameObject monsterPrefab = PrefabUtility.GetCorrespondingObjectFromSource<GameObject>(childTransform.gameObject);
+				if (monsterPrefab == null)
+					continue;
+				string monsterPrefabPath = AssetDatabase.GetAssetPath(monsterPrefab);
+				if (string.IsNullOrEmpty(monsterPrefabPath))
+					continue;
+				if (monsterPrefabPath.Contains("Assets/Resource/Monster") == false)
+					continue;
+
+				SpawnFlag.SpawnInfo info = new SpawnFlag.SpawnInfo();
+				info.prefab = monsterPrefab;
+				info.localPosition = childTransform.localPosition;
+				info.localRotation = childTransform.localRotation.eulerAngles;
+				info.localScale = childTransform.localScale;
+				spawnFlagPrefabComponent._listSpawnInfo.Add(info);
+			}
+
+			PrefabUtility.SavePrefabAsset(prefab);
+		}
+	}
+}
--- Assets/Source/Map/SpawnFlag.cs
+++ Assets/Source/Map/SpawnFlag.cs
@@ -1,18 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
+[ExecuteInEditMode]
 public class SpawnFlag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Transform playerSpawnTransform;
 
-    // Update is called once per frame
-    void Update()
+	[Serializable]
+	public class SpawnInfo
+	{
+		public GameObject prefab;
+		public Vector3 localPosition;
+		public Vector3 localRotation;
+		public Vector3 localScale;
+	}
+	public List<SpawnInfo> _listSpawnInfo;
+
+#if UNITY_EDITOR
+	bool _editorSpawned = false;
+#endif
+	void Awake()
+	{
+#if UNITY_EDITOR
+		if (Application.isPlaying == false)
+		{
+			Spawn(true);
+			_editorSpawned = true;
+		}
+#endif
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+#if UNITY_EDITOR
+		if (_editorSpawned)
+			return;
+#endif
+		Spawn();
+	}
+
+	void Spawn(bool editorSpawn = false)
+	{
+		if (_listSpawnInfo == null)
+			return;
+
+		for (int i = 0; i < _listSpawnInfo.Count; ++i)
+		{
+#if UNITY_EDITOR
+			GameObject newObject = null;
+			if (editorSpawn)
+				newObject = PrefabUtility.InstantiatePrefab((UnityEngine.Object)_listSpawnInfo[i].prefab, cachedTransform) as GameObject;
+			else
+				newObject = Instantiate(_listSpawnInfo[i].prefab, cachedTransform);
+#else
+			GameObject newObject = Instantiate(_listSpawnInfo[i].prefab, cachedTransform);
+#endif
+			newObject.transform.localPosition = _listSpawnInfo[i].localPosition;
+			newObject.transform.localRotation = Quaternion.Euler(_listSpawnInfo[i].localRotation);
+			newObject.transform.localScale = _listSpawnInfo[i].localScale;
+		}
+	}
+
+
+
+
+
+
+	Transform _transform;
+	public Transform cachedTransform
+	{
+		get
+		{
+			if (_transform == null)
+				_transform = GetComponent<Transform>();
+			return _transform;
+		}
+	}
 }

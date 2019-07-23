d5cabb5cd7145f9b7de4abfa932de9a1fb64c383
hayannal2009 <hayannal2009@gmail.com>
2019-07-23 21:58:06 +0900

    배틀테스트툴 껍데기 만들어서 등록. 플레이어 AI 제어하는거부터 해봄

---
+++ Assets/Source/Editor/BattleTestTool.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleTestTool : EditorWindow
+{
+	[MenuItem("Window/Open Battle Test Window _F3")]
+	static void Init()
+	{
+		EditorWindow.GetWindow<BattleTestTool>();
+	}
+
+	GUIContent guiContentTitle = new GUIContent("Battle Tool");
+	void OnEnable()
+	{
+		titleContent = guiContentTitle;
+		minSize = new Vector2(200, 400);
+	}
+
+	Color m_DefaultToolColor = new Color(0.8f, 0.8f, 0.8f);
+	Color m_DefaultToolBackgroundColor = Color.white;
+	
+	string _notificationMsg = "Runs in Play mode!";
+	GUIContent _notification = new GUIContent("Runs in Play mode!");
+	void OnGUI()
+	{
+		if (EditorApplication.isPlayingOrWillChangePlaymode)
+		{
+		}
+
+		if (!Application.isPlaying)
+		{
+			//EditorApplication.isPlaying = true;
+			EditorGUILayout.HelpBox(_notificationMsg, MessageType.Info);
+			ShowNotification(_notification);
+			return;
+		}
+
+		OnGUI_Player();
+	}
+
+	PlayerAI _playerAI = null;
+	bool usePlayerAI = true;
+	void OnGUI_Player()
+	{
+		GUILayout.BeginVertical("box");
+		{
+			Color defaultColor = GUI.color;
+			GUI.color = Color.cyan;
+			string szDesc = string.Format("Player");
+			EditorGUILayout.LabelField(szDesc, EditorStyles.textField);
+			GUI.color = defaultColor;
+
+			usePlayerAI = EditorGUILayout.Toggle("Toggle Player AI :", usePlayerAI);
+			if (_playerAI == null)
+				_playerAI = GameObject.FindObjectOfType<PlayerAI>();
+			if (_playerAI != null)
+				_playerAI.enabled = usePlayerAI;
+		}
+		GUILayout.EndVertical();	
+	}
+}

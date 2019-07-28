defdef88aefad58b60164305605cd62d55797bf0
hayannal2009 <hayannal2009@gmail.com>
2019-07-29 01:46:47 +0900

    ObjectUtil 헬퍼 클래스 가져온다.

---
+++ Assets/Framework/Util/ObjectUtil.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectUtil : MonoBehaviour
+{
+	public static void ChangeLayer(GameObject obj, int layer)
+	{
+		obj.layer = layer;
+		for (int i = 0; i < obj.transform.childCount; ++i)
+			ChangeLayer(obj.transform.GetChild(i).gameObject, layer);
+	}
+}

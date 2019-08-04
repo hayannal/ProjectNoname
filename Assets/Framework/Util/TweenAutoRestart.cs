152c33a08f6fc1fee114c27ee20316116fdce203
hayannal2009 <hayannal2009@gmail.com>
2019-08-04 21:01:10 +0900

    Tween Auto Restart 스크립트 추가. 어차피 오브젝트 안에 붙은거 다 돌릴테니 GetComponent 사용하게 수정.
    GetComponentsInChildren 뽑아와서 실행하면 중복실행하는거다.

---
+++ Assets/Framework/Util/TweenAutoRestart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TweenAutoRestart : MonoBehaviour {
+
+    DOTweenAnimation _tweenAnimation;
+
+	// Use this for initialization
+	void Start () {
+        _tweenAnimation = GetComponent<DOTweenAnimation>();
+	}
+	
+    void OnEnable()
+    {
+		if (_tweenAnimation == null)
+			return;
+
+		if (_tweenAnimation.autoPlay)
+			_tweenAnimation.DORestart();
+	}
+}

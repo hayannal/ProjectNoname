d3539ede95032a8b8f043889b67c9c65439af2e1
hayannal2009 <hayannal2009@gmail.com>
2019-08-14 01:14:49 +0900

    Hexart UI 에서 디폴트버튼 뽑아낼때 추가한 스크립트 커밋하는거 빠뜨림. 커밋.

---
+++ Assets/Source/UI/CustomButtonAnimator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomButtonAnimator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler
+{
+	public string pressedStateName = "Pressed";
+	public string pressed2NormalStateName = "Pressed to Normal";
+
+	Animator buttonAnimator;
+	int pressedStateValue;
+	int pressed2NormalStateValue;
+
+	void Awake()
+	{
+		buttonAnimator = GetComponent<Animator>();
+		pressedStateValue = BattleInstanceManager.instance.GetActionNameHash(pressedStateName);
+		pressed2NormalStateValue = BattleInstanceManager.instance.GetActionNameHash(pressed2NormalStateName);
+	}
+
+	bool _pressed = false;
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		buttonAnimator.CrossFade(pressedStateValue, 0.05f);
+		_pressed = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		buttonAnimator.CrossFade(pressed2NormalStateValue, 0.05f);
+		_pressed = false;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		if (_pressed)
+			buttonAnimator.CrossFade(pressed2NormalStateValue, 0.05f);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		if (_pressed)
+			buttonAnimator.CrossFade(pressedStateValue, 0.05f);
+	}
+}

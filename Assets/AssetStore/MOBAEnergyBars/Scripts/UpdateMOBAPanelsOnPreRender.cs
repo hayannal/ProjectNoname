443313ad937c40538c90f4014529e04057c03359
hayannal2009 <hayannal2009@gmail.com>
2019-08-16 17:17:59 +0900

    MOBA HealthBar 에셋 추가.

---
+++ Assets/AssetStore/MOBAEnergyBars/Scripts/UpdateMOBAPanelsOnPreRender.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UpdateMOBAPanelsOnPreRender : MonoBehaviour {
+    public delegate void CallOnPreRender();
+    public CallOnPreRender CallList;
+
+    void OnPreRender()
+    {
+        if (CallList != null) CallList();
+    }
+}
---
+++ Assets/AssetStore/MOBAEnergyBars/Scripts/WorldSpacePanel.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldSpacePanel : MonoBehaviour {
+    public Camera Camera;
+    public bool FaceCamera = true;
+
+    Vector3 offset;
+    GameObject lastCamera;
+    bool preRenderCallback;
+
+    Camera cam { get { return (Camera != null) ? Camera : Camera.main; } set { Camera = value; } }
+
+    public void DoUpdate()
+    {
+        doFollow();
+    }
+
+    void Awake()
+    {
+        preRenderCallback = false;
+    }
+
+    void Start()
+    {
+        offset = transform.localPosition;
+    }
+
+    void Update()
+    {
+        updatePreRenderRegistration();
+    }
+
+    void LateUpdate()
+    {
+        if (!preRenderCallback)
+        {
+            DoUpdate();
+        }
+    }
+
+    void OnDestroy()
+    {
+        removePreRenderRegistration();
+    }
+
+    void updatePreRenderRegistration()
+    {
+        if (lastCamera != cam)
+        {
+            removePreRenderRegistration();
+            UpdateMOBAPanelsOnPreRender pr = cam.GetComponent<UpdateMOBAPanelsOnPreRender>();
+            if (pr != null)
+            {
+                pr.CallList += DoUpdate;
+                preRenderCallback = true;
+            }
+            lastCamera = cam.gameObject;
+        }
+    }
+
+    void removePreRenderRegistration()
+    {
+        if (lastCamera != null)
+        {
+            UpdateMOBAPanelsOnPreRender pr = lastCamera.GetComponent<UpdateMOBAPanelsOnPreRender>();
+            if (pr != null)
+            {
+                pr.CallList -= DoUpdate;
+            }
+        }
+        preRenderCallback = false;
+    }
+
+    void doFollow()
+    {
+        if (FaceCamera)
+        {
+            transform.rotation = Quaternion.identity;
+            if (transform.parent != null)
+                transform.position = transform.parent.position + offset;
+            else
+                transform.position = offset;
+            transform.LookAt(cam.transform, cam.transform.rotation * Vector3.up);
+        }
+    }
+}

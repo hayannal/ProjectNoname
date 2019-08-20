d5ea5722fbabd6b8dedc683bbbf3ac859df09e24
hayannal2009 <hayannal2009@gmail.com>
2019-08-20 12:52:10 +0900

    transform 캐싱하게 수정

--- Assets/AssetStore/FantasyEnvironmentPack/Scripts/Rotation.cs
+++ Assets/AssetStore/FantasyEnvironmentPack/Scripts/Rotation.cs
@@ -5,18 +5,21 @@ public class Rotation : MonoBehaviour {
 
     public float speed = 0;
     public bool y = false;
+
+	Transform _transform;
+
 	// Use this for initialization
 	void Start () {
-	
+		_transform = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         if (!y)
-        transform.Rotate(0,0,speed*Time.deltaTime);
+        _transform.Rotate(0,0,speed*Time.deltaTime);
         if (y)
-            transform.Rotate(0, speed * Time.deltaTime, 0);
+            _transform.Rotate(0, speed * Time.deltaTime, 0);
 
     }
 }

670caada56dafbd704d31bd6d1276d8c26d0bb93
hayannal2009 <hayannal2009@gmail.com>
2019-08-22 18:29:35 +0900

    밤낮 제어를 위해 EnvironmentSetting 프리팹 만들고 필수 변경 항목들을 등록해둔다. 프리팹도 종류별로 만들어둔다.
    동시에 하나만 존재하면 된다. 꺼놨다가 다시 켜는것도 가능

---
+++ Assets/Framework/Renderer/EnvironmentSetting.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EnvironmentSetting : MonoBehaviour
+{
+	[ColorUsageAttribute(false, true)]
+	public Color ambientColor = Color.grey;
+	public float bloomThreshold = 1.3f;
+
+	// for disable caching
+	void OnEnable()
+	{
+		if (!_started)
+			return;
+
+		SetEnvironment();
+	}
+
+	Light _directionalLight;
+	void Awake()
+	{
+		_directionalLight = GetComponent<Light>();
+	}
+
+	bool _started;
+	void Start()
+	{
+		SetEnvironment();
+
+#if UNITY_EDITOR
+		_prevAmbientColor = ambientColor;
+		_prevBloomThreshold = bloomThreshold;
+#endif
+		_started = true;
+	}
+
+	void SetEnvironment()
+	{
+		SetSun();
+		SetAmbientColor();
+		SetBloomThreshold();
+	}
+
+	void SetSun()
+	{
+		RenderSettings.sun = _directionalLight;
+	}
+
+	void SetAmbientColor()
+	{
+		RenderSettings.ambientLight = ambientColor;
+	}
+
+	void SetBloomThreshold()
+	{
+		if (CustomRenderer.instance != null)
+		{
+			RFX4_MobileBloom bloom = CustomRenderer.instance.GetComponent<RFX4_MobileBloom>();
+			bloom.bloomThreshold = bloomThreshold;
+		}
+	}
+
+#if UNITY_EDITOR
+	Color _prevAmbientColor;
+	float _prevBloomThreshold;
+	void Update()
+	{
+		if (_prevAmbientColor != ambientColor)
+		{
+			SetAmbientColor();
+			_prevAmbientColor = ambientColor;
+		}
+
+		if (_prevBloomThreshold != bloomThreshold)
+		{
+			SetBloomThreshold();
+			_prevBloomThreshold = bloomThreshold;
+		}
+	}
+#endif
+}

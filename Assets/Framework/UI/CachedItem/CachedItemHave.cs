c31326724c423734e98fc0be07e03a141e611c86
hayannal2009 <hayannal2009@gmail.com>
2019-08-25 14:52:16 +0900

    UI 자식 오브젝트들을 위한 CachedItem 구조 추가. 상속이랑 Have 두개다 올려둔다.
    have 위주로 사용하게 될듯

---
+++ Assets/Framework/UI/CachedItem/CachedItemHave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CachedItemHave<T> where T : MonoBehaviour
+{
+	List<T> _listCachedItem = new List<T>();
+
+	public T GetCachedItem(GameObject cachedObjectPrefab, Transform parentTransform)
+	{
+		for (int i = 0; i < _listCachedItem.Count; ++i)
+		{
+			if (!_listCachedItem[i].gameObject.activeSelf)
+				return _listCachedItem[i];
+		}
+
+		GameObject newObject = Object.Instantiate(cachedObjectPrefab, parentTransform);
+		T newItem = newObject.GetComponent<T>();
+		_listCachedItem.Add(newItem);
+		return newItem;
+	}
+}
---
+++ Assets/Framework/UI/CachedItem/CachedItemMono.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CachedItemMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+	List<T> _listCachedItem = new List<T>();
+
+	public GameObject cachedObjectPrefab;
+
+	public T GetCachedItem(Transform parentTransform)
+	{
+		for (int i = 0; i < _listCachedItem.Count; ++i)
+		{
+			if (!_listCachedItem[i].gameObject.activeSelf)
+				return _listCachedItem[i];
+		}
+
+		GameObject newObject = Instantiate(cachedObjectPrefab, parentTransform);
+		T newItem = newObject.GetComponent<T>();
+		_listCachedItem.Add(newItem);
+		return newItem;
+	}
+}

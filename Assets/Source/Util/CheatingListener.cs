00ef5ec3f192ee3db7d56faf8f84bbe95c954946
hayannal2009 <hayannal2009@gmail.com>
2019-08-29 22:30:49 +0900

    디텍팅 리스너 프리팹으론 안빼고 씬에다만 설치해본다.

---
+++ Assets/Source/Util/CheatingListener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatingListener : MonoBehaviour
+{
+	public void OnDetect()
+	{
+		Application.Quit();
+	}
+}

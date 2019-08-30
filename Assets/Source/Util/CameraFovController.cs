e8d65703e6dfb4a846a6971ebec9bd1d444edad4
hayannal2009 <hayannal2009@gmail.com>
2019-08-30 19:50:48 +0900

    세로에 맞는 fov 컨트롤러 붙여서 커밋. DirtIntensity 도 2로 변경해서 커밋

---
+++ Assets/Source/Util/CameraFovController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFovController : MonoBehaviour
+{
+	public float horizontalFov = 8.0f;
+
+	Camera _mainCamera;
+	int _lastWidth;
+	int _lastHeight;
+
+	void Awake()
+	{
+		_mainCamera = GetComponent<Camera>();
+	}
+
+	void Start()
+	{
+		RefreshFov();
+	}
+
+	public void RefreshFov()
+	{
+		_mainCamera.fieldOfView = horizontalFov / _mainCamera.aspect;
+		_lastWidth = Screen.width;
+		_lastHeight = Screen.height;
+	}
+
+	void Update()
+	{
+		if (_mainCamera == null)
+			return;
+
+		if (_lastWidth != Screen.width || _lastHeight != Screen.height)
+			RefreshFov();
+	}
+}

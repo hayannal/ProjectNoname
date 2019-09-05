6b601f31801b9f63a4947c12e81d5a133ad7f7d1
hayannal2009 <hayannal2009@gmail.com>
2019-09-05 18:29:26 +0900

    DisableObject 코루틴 돌다 씬전환 하면 에러떠서 MEC디파인으로 묶어둔다.

--- Assets/Framework/Util/DisableObject.cs
+++ Assets/Framework/Util/DisableObject.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿#define USE_MEC
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+#if USE_MEC
 using MEC;
+#endif
 
 public class DisableObject : MonoBehaviour {
 
@@ -11,19 +15,36 @@ public class DisableObject : MonoBehaviour {
 	void Start()
 	{
 		started = true;
+#if USE_MEC
 		Timing.RunCoroutine(Disable());
+#else
+		StartCoroutine(Disable(delayTime));
+#endif
 	}
 
 	void OnEnable()
 	{
 		if (!started)
 			return;
+#if USE_MEC
 		Timing.RunCoroutine(Disable());
+#else
+		StartCoroutine(Disable(delayTime));
+#endif
 	}
 
+#if USE_MEC
 	IEnumerator<float> Disable()
+#else
+	IEnumerator Disable(float delayTime)
+#endif
 	{
+#if USE_MEC
 		yield return Timing.WaitForSeconds(delayTime);
+#else
+		yield return new WaitForSeconds(delayTime);
+#endif
+
 		gameObject.SetActive(false);
 	}
 }

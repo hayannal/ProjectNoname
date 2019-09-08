62e9549b74fb6b411373a127d31eecd8519dff37
hayannal2009 <hayannal2009@gmail.com>
2019-09-08 22:04:12 +0900

    LoadAsync 만들까 하다가 AddressableAssetLoad에 콜백 기능을 추가하기로 한다.
    아무래도 오브젝트는 캔버스와 달리 쓰임새가 달라서 1개씩만 만들어놓는 것도 제어하기 불편해서 하지 않는다.

--- Assets/Framework/AddressableAsset/AddressableAssetLoadManager.cs
+++ Assets/Framework/AddressableAsset/AddressableAssetLoadManager.cs
@@ -13,6 +13,7 @@ public class AsyncOperationResult
 {
 	public AsyncOperationHandle<GameObject> handle;
 	public string category;
+	public System.Action<GameObject> callback;
 
 	public bool IsDone
 	{
@@ -60,9 +61,9 @@ public class AddressableAssetLoadManager : MonoBehaviour
 	Dictionary<string, AsyncOperationResult> _dicAddressableAsset = new Dictionary<string, AsyncOperationResult>();
 	Dictionary<string, int> _dicCategoryCount = new Dictionary<string, int>();
 
-	public static AsyncOperationResult GetAddressableAsset(string address, string category = "")
+	public static AsyncOperationResult GetAddressableAsset(string address, string category = "", System.Action<GameObject> callback = null)
 	{
-		return instance.InternalGetAddressableAsset(address, category);
+		return instance.InternalGetAddressableAsset(address, category, callback);
 	}
 
 	public static void ReleaseAll()
@@ -84,20 +85,29 @@ public class AddressableAssetLoadManager : MonoBehaviour
 	// 여기서 카테고리는 번들 단위도 아니고, 그냥 게임 내에서 부르는 로직상의 의미다.
 	// "Map"이라고 오면 "Map"으로 불리워진 에셋의 전체수량을 기억해놨다가 일정량 이상 높아지면 릴리즈 하는 형태로 간다.
 	// 레퍼런스 카운트와 달리 같은걸 호출했다고 +1 하진 않는다.
-	AsyncOperationResult InternalGetAddressableAsset(string address, string category = "")
+	AsyncOperationResult InternalGetAddressableAsset(string address, string category = "", System.Action<GameObject> callback = null)
 	{
 		if (_dicAddressableAsset.ContainsKey(address))
+		{
+			// 사실 여기서 IsDone이 안되어있으면 문제긴 한데 이 콜백은 동시호출도 아니고 천천히 로딩되면 받는거 위주로 쓸거기 때문에 중복호출에 대한 처리까지는 하지 않는다.
+			if (callback != null && _dicAddressableAsset[address].IsDone)
+				callback(_dicAddressableAsset[address].Result);
 			return _dicAddressableAsset[address];
+		}
 
 		AsyncOperationResult asyncOperationResult = new AsyncOperationResult();
 		asyncOperationResult.handle = Addressables.LoadAssetAsync<GameObject>(address);
 		asyncOperationResult.category = category;
+		asyncOperationResult.callback = callback;
 		_dicAddressableAsset.Add(address, asyncOperationResult);
+		if (callback != null)
+			_listAddressableAssetForCallback.Add(asyncOperationResult);
 		return asyncOperationResult;
 	}
 
 	void InternalReleaseAll()
 	{
+		_listAddressableAssetForCallback.Clear();
 		Dictionary<string, AsyncOperationResult>.Enumerator e = _dicAddressableAsset.GetEnumerator();
 		while (e.MoveNext())
 			e.Current.Value.SafeRelease();
@@ -112,6 +122,22 @@ public class AddressableAssetLoadManager : MonoBehaviour
 		// 차후 맵 로딩 10회 후 릴리즈라던가
 		// 카테고리 카운트를 세서 일정량이 넘었을때 그룹별로 해제라던가를 하면 될거다.
 		InternalReleaseAll();
+	}
+	#endregion
+
+	#region Load Callback
+	List<AsyncOperationResult> _listAddressableAssetForCallback = new List<AsyncOperationResult>();
+	void Update()
+	{
+		for (int i = _listAddressableAssetForCallback.Count - 1; i >= 0; --i)
+		{
+			if (_listAddressableAssetForCallback[i].IsDone == false)
+				continue;
+			
+			if (_listAddressableAssetForCallback[i].callback != null)
+				_listAddressableAssetForCallback[i].callback(_listAddressableAssetForCallback[i].Result);
+			_listAddressableAssetForCallback.Remove(_listAddressableAssetForCallback[i]);
+		}
 	}
 	#endregion
 }

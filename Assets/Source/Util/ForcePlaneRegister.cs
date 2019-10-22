efeab2f27112417281125835fc602f2b6533f983
hayannal2009 <hayannal2009@gmail.com>
2019-10-22 15:29:17 +0900

    배틀씬에선 Plane을 꺼내두기 때문에 동일한 테스트를 하려면 직접 등록해야한다. Util클래스 하나 추가.

---
+++ Assets/Source/Util/ForcePlaneRegister.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePlaneRegister : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		BattleInstanceManager.instance.planeCollider = GetComponent<Collider>();
+    }
+}

deeb990a490b44f6932ddfc1b99503a5ab2c827a
hayannal2009 <hayannal2009@gmail.com>
2019-11-01 17:16:50 +0900

    레벨업 UI 3버튼 형태로 수정

--- Assets/Source/UI/ObjectIndicator/LevelUpIndicatorButton.cs
+++ Assets/Source/UI/ObjectIndicator/LevelUpIndicatorButton.cs
@@ -5,25 +5,16 @@ using UnityEngine.UI;
 
 public class LevelUpIndicatorButton : MonoBehaviour
 {
+	public Text nameTextList;
 	public Image iconImage;
-	public Image titleImage;
 	public Coffee.UIExtensions.UIGradient gradient;
-	public Text[] nameTextList;
-	public Text[] descTextList;
 
-	Color normalTitleColor = Color.white;
-	Color normalGradientColor = Color.black;
-	Color exclusiveTitleColor = new Color(1.0f, 0.5f, 0.0f);
-	Color exclusiveGradientColor = new Color(171.0f / 255.0f, 90.0f / 255.0f, 27.0f / 255.0f);
+	Color normalGradientColor = Color.white;
+	Color exclusiveGradientColor = new Color(1.0f, 0.5f, 0.0f);
 
 	public void SetInfo(LevelPackTableData levelPackTableData)
 	{
-		titleImage.color = levelPackTableData.exclusive ? exclusiveTitleColor : normalTitleColor;
-		gradient.color1 = levelPackTableData.exclusive ? exclusiveGradientColor : normalGradientColor;
-
-		for (int i = 0; i < nameTextList.Length; ++i)
-			nameTextList[i].SetLocalizedText(UIString.instance.GetString(levelPackTableData.nameId));
-		for (int i = 0; i < descTextList.Length; ++i)
-			descTextList[i].SetLocalizedText(UIString.instance.GetString(levelPackTableData.descriptionId));
+		nameTextList.SetLocalizedText(UIString.instance.GetString(levelPackTableData.nameId));
+		gradient.color2 = levelPackTableData.exclusive ? exclusiveGradientColor : normalGradientColor;
 	}
 }

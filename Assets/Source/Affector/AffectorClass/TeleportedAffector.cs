0ad2aa0806df9b5101b566d7bf53be6c71c72550
hayannal2009 <hayannal2009@gmail.com>
2019-11-14 01:00:08 +0900

    텔레포트 이펙트 적용 및 applied 변수 넣어서 적용도 안했는데 finalize 호출되는거 막기

--- Assets/Source/Affector/AffectorClass/TeleportedAffector.cs
+++ Assets/Source/Affector/AffectorClass/TeleportedAffector.cs
@@ -9,6 +9,8 @@ public class TeleportedAffector : AffectorBase
 
 	const float TeleportedHeight = 500.0f;
 
+	bool _applied = false;
+	AffectorValueLevelTableData _affectorValueLevelTableData;
 	public override void ExecuteAffector(AffectorValueLevelTableData affectorValueLevelTableData, HitParameter hitParameter)
 	{
 		if (_actor.actorStatus.IsDie())
@@ -20,12 +22,22 @@ public class TeleportedAffector : AffectorBase
 		// lifeTime
 		_endTime = CalcEndTime(affectorValueLevelTableData.fValue1);
 
+		_affectorValueLevelTableData = affectorValueLevelTableData;
+
+		if (!string.IsNullOrEmpty(affectorValueLevelTableData.sValue3))
+		{
+			GameObject onStartEffectPrefab = FindPreloadObject(affectorValueLevelTableData.sValue3);
+			if (onStartEffectPrefab != null)
+				BattleInstanceManager.instance.GetCachedObject(onStartEffectPrefab, _actor.cachedTransform.position, _actor.cachedTransform.rotation);
+		}
+
 		_actor.EnableAI(false);
 		_actor.actionController.idleAnimator.enabled = false;
 		_actor.baseCharacterController.movement.useGravity = false;
 		_prevPosition = _actor.cachedTransform.position;
 		_actor.cachedTransform.position = new Vector3(_actor.cachedTransform.position.x, TeleportedHeight, _actor.cachedTransform.position.z);
 		Push(this);
+		_applied = true;
 	}
 
 	public override void UpdateAffector()
@@ -36,12 +48,23 @@ public class TeleportedAffector : AffectorBase
 
 	public override void FinalizeAffector()
 	{
+		if (_applied == false)
+			return;
+
 		Pop(this);
 		_actor.cachedTransform.position = _prevPosition;
 		_actor.baseCharacterController.movement.useGravity = true;
 		_actor.actionController.idleAnimator.enabled = true;
 		_actor.actionController.PlayActionByActionName("Idle");
 		_actor.EnableAI(true);
+
+		if (!string.IsNullOrEmpty(_affectorValueLevelTableData.sValue4))
+		{
+			GameObject onEndEffectPrefab = FindPreloadObject(_affectorValueLevelTableData.sValue4);
+			if (onEndEffectPrefab != null)
+				BattleInstanceManager.instance.GetCachedObject(onEndEffectPrefab, _actor.cachedTransform.position, _actor.cachedTransform.rotation);
+		}
+		_applied = false;
 	}
 
 	#region static

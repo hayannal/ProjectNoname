9b980ce8a40f7aaf2b90f97571aea64c95c2b32a
hayannal2009 <hayannal2009@gmail.com>
2019-11-13 23:57:36 +0900

    이동불가 이펙트 적용해봄

--- Assets/Source/Affector/AffectorClass/CannotMoveAffector.cs
+++ Assets/Source/Affector/AffectorClass/CannotMoveAffector.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class CannotMoveAffector : AffectorBase
 {
-	// 해당 어펙터를 쓰려면 액터에다가 본 추가로 넣고 액터 preload 오브젝트 리스트에다가 루프 이펙트나 피격 이벤트 이펙트를 넣어놔야한다.
-	static string BONE_NAME = "Bone_CannotMove_";
-
 	float _endTime;
-	Transform _boneTransform;
 	Transform _loopEffectTransform;
 
 	public override void ExecuteAffector(AffectorValueLevelTableData affectorValueLevelTableData, HitParameter hitParameter)
@@ -21,20 +18,13 @@ public class CannotMoveAffector : AffectorBase
 		// lifeTime
 		_endTime = CalcEndTime(affectorValueLevelTableData.fValue1);
 
-		// attach bone
-		if (_actor != null)
-			_boneTransform = _actor.actionController.dummyFinder.FindTransform(BONE_NAME);
-		else
-			_boneTransform = _affectorProcessor.cachedTransform;
-
 		// loop effect
 		GameObject loopEffectPrefab = FindPreloadObject(affectorValueLevelTableData.sValue3);
 		if (loopEffectPrefab != null)
 		{
-			_loopEffectTransform = BattleInstanceManager.instance.GetCachedObject(loopEffectPrefab, _boneTransform).transform;
-			_loopEffectTransform.localPosition = Vector3.zero;
-			_loopEffectTransform.localRotation = Quaternion.identity;
-			_loopEffectTransform.localScale = Vector3.one;
+			_loopEffectTransform = BattleInstanceManager.instance.GetCachedObject(loopEffectPrefab, _actor.cachedTransform.position, _actor.cachedTransform.rotation).transform;
+			_loopEffectTransform.localScale = Vector3.zero;
+			_loopEffectTransform.DOScale(1.0f, 0.4f).SetEase(Ease.OutQuad);
 		}
 
 		_actor.actorStatus.OnChangedStatus(ActorStatusDefine.eActorStatus.MoveSpeed);
@@ -52,15 +42,25 @@ public class CannotMoveAffector : AffectorBase
 			_actor.actorStatus.OnChangedStatus(ActorStatusDefine.eActorStatus.MoveSpeed);
 			return;
 		}
+
+		if (_actor.actorStatus.IsDie())
+		{
+			finalized = true;
+			return;
+		}
+
+		if (_loopEffectTransform != null)
+			_loopEffectTransform.position = _actor.cachedTransform.position;
 	}
 
 	public override void FinalizeAffector()
 	{
 		if (_loopEffectTransform != null)
 		{
-			DisableParticleEmission.DisableEmission(_loopEffectTransform);
-			//_loopEffectTransform.gameObject.SetActive(false);
-			_loopEffectTransform = null;
+			_loopEffectTransform.DOScale(0.0f, 0.4f).SetEase(Ease.OutQuad).onComplete = () => {
+				_loopEffectTransform.gameObject.SetActive(false);
+				_loopEffectTransform = null;
+			};
 		}
 	}
 }

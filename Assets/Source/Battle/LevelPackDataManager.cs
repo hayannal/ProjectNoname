6a5d649561cc7cc069dcb455c7c85b7e63e387e0
hayannal2009 <hayannal2009@gmail.com>
2019-11-15 21:30:53 +0900

    코드 중복 막기위해 서브함수로 빼둔다.

--- Assets/Source/Battle/LevelPackDataManager.cs
+++ Assets/Source/Battle/LevelPackDataManager.cs
@@ -25,33 +25,28 @@ public class LevelPackDataManager : MonoBehaviour
 		for (int i = 0; i < TableDataManager.instance.levelPackTable.dataArray.Length; ++i)
 		{
 			LevelPackTableData levelPackTableData = TableDataManager.instance.levelPackTable.dataArray[i];
-			if (levelPackTableData.exclusive == false)
-			{
-				listLevelPackTableData.Add(levelPackTableData);
-				continue;
-			}
-
-			// exclusive 예외처리 추가.
-			if (levelPackTableData.useActor.Length == 1 && levelPackTableData.useActor[0] == "All")
-			{
-				listLevelPackTableData.Add(levelPackTableData);
-				continue;
-			}
-
-			bool find = false;
-			for (int j = 0; j < levelPackTableData.useActor.Length; ++j)
-			{
-				if (levelPackTableData.useActor[j] == actorId)
-				{
-					find = true;
-					break;
-				}
-			}
-			if (find)
+			if (IsAcquirableLevelPack(levelPackTableData, actorId))
 				listLevelPackTableData.Add(levelPackTableData);
 		}
 		_dicActorLevelPack.Add(actorId, listLevelPackTableData);
 		return listLevelPackTableData;
+	}
+
+	bool IsAcquirableLevelPack(LevelPackTableData levelPackTableData, string actorId)
+	{
+		if (levelPackTableData.exclusive == false)
+			return true;
+
+		// exclusive 예외처리 추가.
+		if (levelPackTableData.useActor.Length == 1 && levelPackTableData.useActor[0] == "All")
+			return true;
+
+		for (int i = 0; i < levelPackTableData.useActor.Length; ++i)
+		{
+			if (levelPackTableData.useActor[i] == actorId)
+				return true;
+		}
+		return false;
 	}
 
 	LevelPackTableData _cachedExclusiveLevelPackTableDataAfterMax;

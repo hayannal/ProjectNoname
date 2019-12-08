8c32196007e4767a8f8383e37f5940a96b02d630
hayannal2009 <hayannal2009@gmail.com>
2019-12-08 20:43:43 +0900

    마을 로비도 껐다켜질 수 있어짐에 따라 OnDisable 처리 해둔다.

--- Assets/Source/UI/ObjectIndicator/TreasureChestIndicatorCanvas.cs
+++ Assets/Source/UI/ObjectIndicator/TreasureChestIndicatorCanvas.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreasureChestIndicatorCanvas : ObjectIndicatorCanvas
 {
+	public GameObject buttonRootObject;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		GetComponent<Canvas>().worldCamera = UIInstanceManager.instance.GetCachedCameraMain();
+	}
+
+	void OnEnable()
+	{
 		InitializeTarget(targetTransform);
+	}
+
+	void OnDisable()
+	{
+		buttonRootObject.SetActive(false);
 	}
 
 	public void OnClickButton()

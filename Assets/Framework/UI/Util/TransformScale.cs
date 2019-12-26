86e0415da91283f0535f91af2e4e0e082f7a5648
hayannal2009 <hayannal2009@gmail.com>
2019-12-26 18:04:59 +0900

    등장시 스케일 애니 자주 쓸거같아서 framework쪽에 추가

---
+++ Assets/Framework/UI/Util/TransformScale.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TransformScale : MonoBehaviour
+{
+	public float downScale = 0.9f;
+	public float startScale = 1.1f;
+	public float startAnimationDuration = 0.2f;
+	public bool ignoreTimeScale;
+
+	Transform _transform;
+	void Awake()
+	{
+		_transform = transform;
+	}
+
+	void OnEnable()
+	{
+		PlayStartAnimation();
+	}
+
+	void PlayStartAnimation()
+	{
+		_transform.localScale = new Vector3(downScale, downScale, 1.0f);
+		if (ignoreTimeScale)
+			_transform.DOScale(startScale, startAnimationDuration * 0.5f).SetEase(Ease.OutQuad).OnComplete(OnCompleteScale).SetUpdate(true);
+		else
+			_transform.DOScale(startScale, startAnimationDuration * 0.5f).SetEase(Ease.OutQuad).OnComplete(OnCompleteScale);
+	}
+
+	void OnCompleteScale()
+	{
+		if (ignoreTimeScale)
+			_transform.DOScale(1.0f, startAnimationDuration * 0.5f).SetEase(Ease.OutQuad).SetUpdate(true);
+		else
+			_transform.DOScale(1.0f, startAnimationDuration * 0.5f).SetEase(Ease.OutQuad);
+	}
+}

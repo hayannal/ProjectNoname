034affd647f01c3044ed52b907cb4e834a574d58
hayannal2009 <hayannal2009@gmail.com>
2020-01-04 20:03:35 +0900

    17번 Kachujin 임시 인벤에 추가. 어드레스 연결

--- Assets/Source/PlayerData/PlayerData.cs
+++ Assets/Source/PlayerData/PlayerData.cs
@@ -128,6 +128,10 @@ public class PlayerData : MonoBehaviour
 		characterData.actorId = "Actor016";
 		characterData.powerLevel = 1;
 		_listCharacterData.Add(characterData);
+		characterData = new CharacterData();
+		characterData.actorId = "Actor017";
+		characterData.powerLevel = 1;
+		_listCharacterData.Add(characterData);
 	}
 
 	public CharacterData GetCharacterData(string actorId)

995b3fccb426ade6f6836b1cb1b7eb9ea15e6118
hayannal2009 <hayannal2009@gmail.com>
2020-01-04 15:35:48 +0900

    SteampunkRobot 캐릭터 프리팹 추가. 어드레스 연결. 임시 인벤에도 추가.

--- Assets/Source/PlayerData/PlayerData.cs
+++ Assets/Source/PlayerData/PlayerData.cs
@@ -124,6 +124,10 @@ public class PlayerData : MonoBehaviour
 		characterData.actorId = "Actor015";
 		characterData.powerLevel = 1;
 		_listCharacterData.Add(characterData);
+		characterData = new CharacterData();
+		characterData.actorId = "Actor016";
+		characterData.powerLevel = 1;
+		_listCharacterData.Add(characterData);
 	}
 
 	public CharacterData GetCharacterData(string actorId)

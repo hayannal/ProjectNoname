672488b4419f5f9fbdbc30201221fca314a7f5e0
hayannal2009 <hayannal2009@gmail.com>
2020-01-06 11:27:41 +0900

    RpgKnight 캐릭터 프리팹 추가. 어드레스 연결. 이펙트도 연결. 임시 인벤에도 추가

--- Assets/Source/PlayerData/PlayerData.cs
+++ Assets/Source/PlayerData/PlayerData.cs
@@ -156,6 +156,10 @@ public class PlayerData : MonoBehaviour
 		characterData.actorId = "Actor024";
 		characterData.powerLevel = 1;
 		_listCharacterData.Add(characterData);
+		characterData = new CharacterData();
+		characterData.actorId = "Actor025";
+		characterData.powerLevel = 1;
+		_listCharacterData.Add(characterData);
 	}
 
 	public CharacterData GetCharacterData(string actorId)

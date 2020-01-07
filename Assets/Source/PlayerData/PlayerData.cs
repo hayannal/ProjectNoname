c11c96d809efcaebd186ddfc309f305155a71e85
hayannal2009 <hayannal2009@gmail.com>
2020-01-07 18:16:46 +0900

    NecromancerFour 캐릭터 프리팹 추가. 어드레스 연결. 임시 인벤에도 추가. 물리 적용

--- Assets/Source/PlayerData/PlayerData.cs
+++ Assets/Source/PlayerData/PlayerData.cs
@@ -184,6 +184,10 @@ public class PlayerData : MonoBehaviour
 		characterData.actorId = "Actor033";
 		characterData.powerLevel = 1;
 		_listCharacterData.Add(characterData);
+		characterData = new CharacterData();
+		characterData.actorId = "Actor036";
+		characterData.powerLevel = 1;
+		_listCharacterData.Add(characterData);
 	}
 
 	public CharacterData GetCharacterData(string actorId)

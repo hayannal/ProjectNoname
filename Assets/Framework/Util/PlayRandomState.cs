c13e12f8fb77d4b1790915a0ce49a371f9c26c3b
hayannal2009 <hayannal2009@gmail.com>
2020-01-28 13:25:14 +0900

    PlayRandomState를 State Machine에 붙이는게 아니라 별도의 Random State에 붙이는거로 변경

--- Assets/Framework/Util/PlayRandomState.cs
+++ Assets/Framework/Util/PlayRandomState.cs
@@ -63,8 +63,15 @@ public class PlayRandomState : StateMachineBehaviour
 	// OnStateExit is called before OnStateExit is called on any state inside this state machine
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
-		if (_lastState == stateInfo.fullPathHash && stateInfo.normalizedTime != 0.0f)
-			_lastState = 0;
+		//if (_lastState == stateInfo.fullPathHash && stateInfo.normalizedTime != 0.0f)
+		//	_lastState = 0;
+
+		// State Machine에 붙여놨더니 안에 들어있는 State들에 전부 적용되서 안에 들어있는 State들 호출될때마다
+		// PlayRandomState Enter가 호출되는거였다.
+		// 그래서 State Machine에 붙이는 구조를 버리고
+		// 외부에 따로 State를 둔 후(Motion은 아예 할당하지 않음) 여기에 PlayRandomState를 붙이기로 한다.
+		// 이 State는 호출되자마자 랜덤하게 바꾸는 역할만 한다.(애니가 실행되진 않는다.)
+		_lastState = 0;
 	}
 
 	// OnStateMove is called before OnStateMove is called on any state inside this state machine

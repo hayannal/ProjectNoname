f03ef9085b4f6351cdcb5b415e47efc7fff36c73
hayannal2009 <hayannal2009@gmail.com>
2020-02-13 22:01:25 +0900

    공용Util에 Shuffle 함수 추가.

--- Assets/Framework/Util/ObjectUtil.cs
+++ Assets/Framework/Util/ObjectUtil.cs
@@ -85,4 +85,17 @@ public class ObjectUtil : MonoBehaviour
 		}
 		return comp as T;
 	}
+
+	public static List<T> Shuffle<T>(List<T> list)
+	{
+		for (int i = 0; i < list.Count; i++)
+		{
+			T temp = list[i];
+			int randomIndex = UnityEngine.Random.Range(i, list.Count);
+			list[i] = list[randomIndex];
+			list[randomIndex] = temp;
+		}
+
+		return list;
+	}
 }

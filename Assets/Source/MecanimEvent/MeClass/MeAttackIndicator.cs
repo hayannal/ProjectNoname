a12e10c3ce075314b313e42ad58c899bd6b1160c
hayannal2009 <hayannal2009@gmail.com>
2020-02-16 17:00:31 +0900

    Attack Indicator Local, Target, Rush 직선거리 세가지 타입으로 나눠둔다.

--- Assets/Source/MecanimEvent/MeClass/MeAttackIndicator.cs
+++ Assets/Source/MecanimEvent/MeClass/MeAttackIndicator.cs
@@ -7,6 +7,14 @@ using UnityEditor;
 
 public class MeAttackIndicator : MecanimEventBase
 {
+	public enum eCreatePositionType
+	{
+		LocalPosition,
+		TargetPosition,
+		RushPosition,
+		//WorldPosition,
+	}
+
 	override public bool RangeSignal { get { return true; } }
 
 	public AttackIndicator.eIndicatorType indicatorType;
@@ -14,6 +22,8 @@ public class MeAttackIndicator : MecanimEventBase
 	public Vector3 offset;
 	public Vector3 startDirection = Vector3.forward;
 	public float areaRadius = 1.0f;
+	public bool attachPrefab = true;
+	public eCreatePositionType createPositionType;
 	public float lineMaxDistance;
 	public float lineWidth = 1.0f;
 	public float sphereCastRadius = 0.2f;
@@ -26,14 +36,31 @@ public class MeAttackIndicator : MecanimEventBase
 	{
 		indicatorType = (AttackIndicator.eIndicatorType)EditorGUILayout.EnumPopup("Attack Indicator Type :", indicatorType);
 		attackIndicatorPrefab = (GameObject)EditorGUILayout.ObjectField("Object :", attackIndicatorPrefab, typeof(GameObject), false);
-		offset = EditorGUILayout.Vector3Field("Offset :", offset);
-		startDirection = EditorGUILayout.Vector3Field("Direction :", startDirection);
+		
 		if (indicatorType == AttackIndicator.eIndicatorType.Prefab)
 		{
 			areaRadius = EditorGUILayout.FloatField("Area Radius :", areaRadius);
+			attachPrefab = EditorGUILayout.Toggle("Attach Prefab :", attachPrefab);
+			createPositionType = (eCreatePositionType)EditorGUILayout.EnumPopup("Create Position Type :", createPositionType);
+			switch (createPositionType)
+			{
+				case eCreatePositionType.LocalPosition:
+				case eCreatePositionType.TargetPosition:
+					offset = EditorGUILayout.Vector3Field("Offset :", offset);
+					break;
+				case eCreatePositionType.RushPosition:
+					offset.z = EditorGUILayout.FloatField("Distance Offset :", offset.z);
+					break;
+			}
+			startDirection = EditorGUILayout.Vector3Field("Direction :", startDirection);
 		}
 		else if (indicatorType == AttackIndicator.eIndicatorType.Line)
 		{
+			attachPrefab = true;
+			createPositionType = eCreatePositionType.LocalPosition;
+			offset = EditorGUILayout.Vector3Field("Offset :", offset);
+			startDirection = EditorGUILayout.Vector3Field("Direction :", startDirection);
+
 			lineMaxDistance = EditorGUILayout.FloatField("Max Distance :", lineMaxDistance);
 			lineWidth = EditorGUILayout.FloatField("Width :", lineWidth);
 			sphereCastRadius = EditorGUILayout.FloatField("SphereCast Radius :", sphereCastRadius);
@@ -45,25 +72,78 @@ public class MeAttackIndicator : MecanimEventBase
 #endif
 
 	AttackIndicator _attackIndicator;
-	Transform _spawnTransform;
+	Actor _actor = null;
 	bool _waitEnd = false;
 	override public void OnRangeSignalStart(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
-		if (_spawnTransform == null)
+		if (_actor == null)
 		{
 			if (animator.transform.parent != null)
-				_spawnTransform = animator.transform.parent;
-			if (_spawnTransform == null)
-				_spawnTransform = animator.transform;
+				_actor = animator.transform.parent.GetComponent<Actor>();
+			if (_actor == null)
+				_actor = animator.GetComponent<Actor>();
 		}
-		if (_spawnTransform != null)
+		if (_actor == null)
+			return;
+
+		if (_attackIndicator == null)
 		{
-			if (_attackIndicator == null)
+			Vector3 createPosition = Vector3.zero;
+			Quaternion createRotation = Quaternion.identity;
+			Transform parentTransform = null;
+			switch (createPositionType)
 			{
-				_attackIndicator = BattleInstanceManager.instance.GetCachedAttackIndicator(attackIndicatorPrefab, _spawnTransform.TransformPoint(offset), Quaternion.LookRotation(_spawnTransform.TransformDirection(startDirection)), _spawnTransform);
-				_attackIndicator.InitializeAttackIndicator(this);
+				case eCreatePositionType.LocalPosition:
+					createPosition = _actor.cachedTransform.TransformPoint(offset);
+					createRotation = Quaternion.LookRotation(_actor.cachedTransform.TransformDirection(startDirection));
+					parentTransform = _actor.cachedTransform;
+					break;
+				case eCreatePositionType.TargetPosition:
+					if (_actor.targetingProcessor.GetTargetCount() > 0)
+					{
+						Collider targetCollider = _actor.targetingProcessor.GetTarget();
+						Transform targetTransform = BattleInstanceManager.instance.GetTransformFromCollider(targetCollider);
+						if (targetTransform != null)
+						{
+							createPosition = targetTransform.TransformPoint(offset);
+							createRotation = Quaternion.LookRotation(targetTransform.TransformDirection(startDirection));
+							parentTransform = targetTransform;
+						}
+					}
+					else
+						return;
+					break;
+				case eCreatePositionType.RushPosition:
+					if (_actor.targetingProcessor.GetTargetCount() > 0)
+					{
+						Collider targetCollider = _actor.targetingProcessor.GetTarget();
+						Transform targetTransform = BattleInstanceManager.instance.GetTransformFromCollider(targetCollider);
+						if (targetTransform != null)
+						{
+							if (offset.z == 0.0f)
+							{
+								createPosition = targetTransform.position;
+							}
+							else
+							{
+								Vector3 diff = targetTransform.position - _actor.cachedTransform.position;
+								float length = diff.magnitude + offset.z;
+								if (length < 0.0f)
+									length = 0.0f;
+								createPosition = _actor.cachedTransform.position + diff.normalized * length;
+							}
+							createRotation = Quaternion.LookRotation(_actor.cachedTransform.TransformDirection(startDirection));
+							parentTransform = _actor.cachedTransform;
+						}
+						else
+							return;
+					}
+					break;
 			}
+			_attackIndicator = BattleInstanceManager.instance.GetCachedAttackIndicator(attackIndicatorPrefab, createPosition, createRotation, attachPrefab ? parentTransform : null);
+			_attackIndicator.InitializeAttackIndicator(this);
 		}
+
 		_waitEnd = true;
 	}
 

9630f5df7d8eee7056634b66bc3daeb6183a6cab
hayannal2009 <hayannal2009@gmail.com>
2020-02-16 16:34:59 +0900

    MoveToTarget 시그널에서 chase 주석처리 하고 이유 적어둔다.

--- Assets/Source/MecanimEvent/MeClass/MeMoveToTarget.cs
+++ Assets/Source/MecanimEvent/MeClass/MeMoveToTarget.cs
@@ -10,13 +10,13 @@ public class MeMoveToTarget : MecanimEventBase
 	override public bool RangeSignal { get { return true; } }
 
 	public float distanceOffset;
-	public bool useChase;
+	//public bool useChase;
 
 #if UNITY_EDITOR
 	override public void OnGUI_PropertyWindow()
 	{
 		distanceOffset = EditorGUILayout.FloatField("Distance Offset :", distanceOffset);
-		useChase = EditorGUILayout.Toggle("Use Chase :", useChase);
+		//useChase = EditorGUILayout.Toggle("Use Chase :", useChase);
 	}
 #endif
 
@@ -79,10 +79,21 @@ public class MeMoveToTarget : MecanimEventBase
 		if (_actor == null)
 			return;
 
-		if (useChase)
-		{
-
-		}
+		// 추적기능이 이론상으로도 조금 애매하긴 하다.
+		// RushAffector와 달리 시간이 고정이기 때문에 추적이 되려면 속도를 바꿔야하는데
+		// 속도는 FixedUpdate에서 처리해야해서 여기서 직접 못하고 VelocityAffector에다가 남은 시간이랑 변경된 속도를 전달해야한다.
+		// 근데 이게 약간의 오차가 생길 수 있어서 - 시간은 deltaTime으로 계산하고 물리는 fixedDeltaTime으로 처리되기 때문.
+		// 추적시 완벽하게 계산해서 따라가기 어렵긴 하다.
+		//
+		// 그리고 같은 직선상 거리에서 멀어지는게 아니라 옆으로 뛸 경우가 더 문제인데
+		// 룩엣을 해서 쫓아가는건 체공 중에 몸을 회전하는거라 이상해보일테고
+		// 룩엣 없이 슬라이드로 횡이동 하는건 바라보는 방향대로 나아가는게 아니라서 역시 이상해보인다.
+		// 어차피 이 MoveToTarget은 하나의 애니 안에서 적에게 다가가는거라 직선으로 가야 제일 자연스러운데
+		// 이걸 깨면서까지 추적기능이 있어야하나 싶어서
+		// 우선은 구현하지 않고 나중에 필요한 순간이 오면 그때 구현하기로 한다.(플레이어용 궁극기..)
+		//if (useChase)
+		//{
+		//}
 	}
 
 	override public void OnRangeSignalEnd(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)

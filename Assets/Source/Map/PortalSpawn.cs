00179f9c8325dca8bf45e878d7e74ad6d934a2b2
hayannal2009 <hayannal2009@gmail.com>
2020-02-21 19:39:55 +0900

    BattleManager보다 뒤에 만들어져야해서 구조 변경

--- Assets/Source/Map/PortalSpawn.cs
+++ Assets/Source/Map/PortalSpawn.cs
@@ -8,7 +8,22 @@ public class PortalSpawn : MonoBehaviour
 	public Transform targetTransform;
 
 	Portal _portal = null;
+
+	bool _started = false;
+	void Start()
+	{
+		InitializePortal();
+		_started = true;
+	}
+
+	// 사실은 OnEnable에서만 해도 되는건데 BattleScene에서 BattleManager보다 나중에 만들어지게 하기 위해 이렇게 처리한다.
 	void OnEnable()
+	{
+		if (_started)
+			InitializePortal();
+	}
+
+	void InitializePortal()
 	{
 		_portal = BattleInstanceManager.instance.GetCachedPortal(cachedTransform.position, Quaternion.identity);
 		_portal.targetPosition = targetTransform.position;

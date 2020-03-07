27e7e4c157779615360649daeca61fbf0f53edbe
hayannal2009 <hayannal2009@gmail.com>
2020-03-07 16:14:55 +0900

    튜토리얼중엔 상점도 열리지 않는다.

--- Assets/Source/UI/ObjectIndicator/TreasureChestIndicatorCanvas.cs
+++ Assets/Source/UI/ObjectIndicator/TreasureChestIndicatorCanvas.cs
@@ -14,6 +14,12 @@ public class TreasureChestIndicatorCanvas : ObjectIndicatorCanvas
 
 	public void OnClickButton()
 	{
+		if (ContentsManager.IsTutorialChapter())
+		{
+			ToastCanvas.instance.ShowToast(UIString.instance.GetString("GameUI_PossibleAfterTraining"), 2.0f);
+			return;
+		}
+
 		Debug.Log("Open shop");
 	}
 }

2a81161e3244f81fbbcbe8f32d00ebbea47b9050
hayannal2009 <hayannal2009@gmail.com>
2020-03-14 21:34:51 +0900

    새챕터 알림 캔버스 추가.

---
+++ Assets/Source/UI/Lobby/NewChapterCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewChapterCanvas : MonoBehaviour
+{
+	public static NewChapterCanvas instance;
+
+	public Text chapterRomanNumberText;
+	public Text chapterNameText;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	public void RefreshChapterInfo(int chapter)
+	{
+		ChapterTableData chapterTableData = TableDataManager.instance.FindChapterTableData(chapter);
+		if (chapterTableData == null)
+			return;
+
+		chapterRomanNumberText.text = SwapCanvas.GetChapterRomanNumberString(chapter);
+		chapterNameText.SetLocalizedText(UIString.instance.GetString(chapterTableData.nameId));
+	}
+}

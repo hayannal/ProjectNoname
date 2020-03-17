f52e6cc2126e5b48f90671e83a793d2564c26653
hayannal2009 <hayannal2009@gmail.com>
2020-03-17 15:03:44 +0900

    SortButton 추가.

---
+++ Assets/Source/UI/Common/SortButton.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SortButton : MonoBehaviour
+{
+	public enum eSortType
+	{
+		PowerLevel,
+		PowerLevelDescending,
+		PowerSource,
+
+		Amount,
+	}
+
+	public Button button;
+	public Text sortText;
+	public CanvasGroup textCanvasGroup;
+	public Canvas buttonTextCanvas;
+	public Canvas backgroundImageCanvas;
+
+	eSortType _sortType = eSortType.PowerLevel;
+	public Action<eSortType> onChangedCallback;
+
+	void Awake()
+	{
+		Canvas parentCanvas = cachedRectTransform.parent.GetComponentInParent<Canvas>();
+		buttonTextCanvas.sortingOrder = parentCanvas.sortingOrder + 2;
+		backgroundImageCanvas.sortingOrder = parentCanvas.sortingOrder + 1;
+	}
+
+	void OnEnable()
+	{
+		textCanvasGroup.alpha = 0.0f;
+	}
+
+	public void OnClickButton()
+	{
+		int index = (int)_sortType;
+		++index;
+		if (index >= (int)eSortType.Amount)
+			index = 0;
+		_sortType = (eSortType)index;
+
+		string stringId = "";
+		switch (_sortType)
+		{
+			case eSortType.PowerLevel: stringId = "GameUI_OrderByPowerLevel"; break;
+			case eSortType.PowerLevelDescending: stringId = "GameUI_OrderByPowerLevelDescending"; break;
+			case eSortType.PowerSource: stringId = "GameUI_OrderByPowerSource"; break;
+		}
+		sortText.SetLocalizedText(UIString.instance.GetString(stringId));
+
+		textCanvasGroup.alpha = 1.0f;
+		_lastClickRemainTime = 2.0f;
+		_fadeTime = 0.0f;
+
+		if (onChangedCallback != null)
+			onChangedCallback.Invoke(_sortType);
+	}
+
+	void Update()
+	{
+		UpdateLastClickTime();
+		UpdateFade();
+	}
+
+	float _lastClickRemainTime;
+	void UpdateLastClickTime()
+	{
+		if (_lastClickRemainTime > 0.0f)
+		{
+			_lastClickRemainTime -= Time.deltaTime;
+			if (_lastClickRemainTime <= 0.0f)
+			{
+				_lastClickRemainTime = 0.0f;
+				_fadeTime = 0.5f;
+			}
+		}
+	}
+
+	float _fadeTime;
+	void UpdateFade()
+	{
+		if (_fadeTime > 0.0f)
+		{
+			_fadeTime -= Time.deltaTime;
+			textCanvasGroup.alpha = _fadeTime * 2.0f;
+			if (_fadeTime <= 0.0f)
+			{
+				_fadeTime = 0.0f;
+				textCanvasGroup.alpha = 0.0f;
+			}
+		}
+	}
+
+	public void SetSortType(eSortType sortType) { _sortType = sortType; }
+
+
+
+	RectTransform _rectTransform;
+	public RectTransform cachedRectTransform
+	{
+		get
+		{
+			if (_rectTransform == null)
+				_rectTransform = GetComponent<RectTransform>();
+			return _rectTransform;
+		}
+	}
+}

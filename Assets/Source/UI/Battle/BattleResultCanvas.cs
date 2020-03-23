a369580f99c9e765cd3e17fc4f246b952a3a9fc9
hayannal2009 <hayannal2009@gmail.com>
2020-03-23 12:42:59 +0900

    액터 획득 표시할때 인벤 체크해서 한다.

--- Assets/Source/UI/Battle/BattleResultCanvas.cs
+++ Assets/Source/UI/Battle/BattleResultCanvas.cs
@@ -101,13 +101,13 @@ public class BattleResultCanvas : MonoBehaviour
 
 		stageValueMaxText.text = string.Format("/ {0}", maxStage);
 
-		if (StageManager.instance.playChapter == 0)
+		if (StageManager.instance.playChapter == 0 && PlayerData.instance.ContainsActor("Actor002") == false)
 		{
 			clearRewardText.SetLocalizedText(UIString.instance.GetString(_clear ? "GameUI_Chp0ClearRewardGot" : "GameUI_Chp0ClearReward"));
 			clearRewardText.gameObject.SetActive(true);
 		}
 
-		if (StageManager.instance.playChapter == 1)
+		if (StageManager.instance.playChapter == 1 && PlayerData.instance.ContainsActor("Actor003") == false)
 		{
 			clearRewardText.SetLocalizedText(UIString.instance.GetString(_clear ? "GameUI_Chp1ClearRewardGot" : "GameUI_Chp1ClearReward"));
 			clearRewardText.gameObject.SetActive(true);

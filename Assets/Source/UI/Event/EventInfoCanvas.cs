c36ddc3e6521886145dccd705b3dab435da9de74
hayannal2009 <hayannal2009@gmail.com>
2020-03-23 14:07:44 +0900

    EventInfoCanvas 추가. 어드레서블도 등록

---
+++ Assets/Source/UI/Event/EventInfoCanvas.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventInfoCanvas : MonoBehaviour
+{
+	public static EventInfoCanvas instance;
+
+	public Text subTitleText;
+	public Text messageText;
+
+	System.Action _okAction;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void ShowCanvas(bool show, string title, string message, string tooltip, System.Action okAction = null)
+	{
+		gameObject.SetActive(show);
+		if (show == false)
+			return;
+
+		subTitleText.SetLocalizedText(title);
+		messageText.SetLocalizedText(message);
+		_tooltip = tooltip;
+		_okAction = okAction;
+	}
+
+	string _tooltip;
+	public void OnClickMoreButton()
+	{
+		TooltipCanvas.Show(true, TooltipCanvas.eDirection.Bottom, _tooltip, 300, subTitleText.transform, new Vector2(0.0f, -35.0f));
+	}
+
+	public void OnClickExitButton()
+	{
+		gameObject.SetActive(false);
+		if (_okAction != null)
+			_okAction();
+	}
+}

8d05c3476bedabca80233b216561fb2fe8997831
hayannal2009 <hayannal2009@gmail.com>
2020-03-25 20:35:03 +0900

    캐릭터정보창 성장탭 말고 다른 탭들도 미리 틀만 만들어둔다.

---
+++ Assets/Source/UI/MainMenu/CharacterInfoPotentialCanvas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterInfoPotentialCanvas : MonoBehaviour
+{
+	public static CharacterInfoPotentialCanvas instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	void Start()
+	{
+		GetComponent<Canvas>().worldCamera = UIInstanceManager.instance.GetCachedCameraMain();
+	}
+
+	void OnEnable()
+	{
+		RefreshInfo();
+	}
+
+	#region Info
+	string _actorId;
+	public void RefreshInfo()
+	{
+		string actorId = CharacterListCanvas.instance.selectedActorId;
+		ActorTableData actorTableData = TableDataManager.instance.FindActorTableData(actorId);
+		
+		_actorId = actorId;
+	}
+	#endregion
+}

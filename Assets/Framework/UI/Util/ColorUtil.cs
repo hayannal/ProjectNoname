df5d4b4b410ce2a21ceee07e3c555db92b792e2e
hayannal2009 <hayannal2009@gmail.com>
2020-03-28 01:59:57 +0900

    halfGray가 필요해져서 Util 클래스 추가.

---
+++ Assets/Framework/UI/Util/ColorUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorUtil : MonoBehaviour
+{
+	public static Color halfGray
+	{
+		get
+		{
+			return (Color.white + Color.gray + Color.gray) * 0.3333f;
+		}
+	}
+}

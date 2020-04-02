f8355cb019e6ef491b72c631d620e4eeed312aa5
hayannal2009 <hayannal2009@gmail.com>
2020-04-02 20:26:41 +0900

    TimeSpaceAltar 인디케이터 추가. 인디케이터는 리소스 공유때문에 InApk Essential에 둔다.

---
+++ Assets/Source/Map/TimeSpaceAltar.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSpaceAltar : MonoBehaviour
+{
+	public int positionIndex;
+
+	void Start()
+	{
+		_position = transform.position;
+	}
+
+	void OnDisable()
+	{
+		if (_objectIndicatorCanvas != null)
+		{
+			_objectIndicatorCanvas.gameObject.SetActive(false);
+			_objectIndicatorCanvas = null;
+		}
+	}
+
+	Vector3 _position;
+	void Update()
+	{
+		if (_spawnedIndicator == false)
+			return;
+		if (_objectIndicatorCanvas == null)
+			return;
+		if (_objectIndicatorCanvas.gameObject.activeSelf == false)
+			return;
+
+		Vector3 diff = BattleInstanceManager.instance.playerActor.cachedTransform.position - _position;
+		diff.y = 0.0f;
+		if ((diff.x * diff.x + diff.z * diff.z) > 2.0f * 2.0f)
+		{
+			_objectIndicatorCanvas.gameObject.SetActive(false);
+			_spawnedIndicator = false;
+		}
+	}
+
+	void ShowIndicator()
+	{
+		AddressableAssetLoadManager.GetAddressableGameObject("TimeSpaceAltarIndicator", "Object", (prefab) =>
+		{
+			if (this == null) return;
+			if (gameObject == null) return;
+			if (gameObject.activeInHierarchy == false) return;
+
+			_objectIndicatorCanvas = (TimeSpaceAltarIndicatorCanvas)UIInstanceManager.instance.GetCachedObjectIndicatorCanvas(prefab);
+			_objectIndicatorCanvas.targetTransform = transform;
+			_objectIndicatorCanvas.positionIndex = positionIndex;
+		});
+
+		_spawnedIndicator = true;
+	}
+
+	TimeSpaceAltarIndicatorCanvas _objectIndicatorCanvas;
+	bool _spawnedIndicator;
+	void OnTriggerEnter(Collider other)
+	{
+		if (_spawnedIndicator)
+			return;
+
+		AffectorProcessor affectorProcessor = BattleInstanceManager.instance.GetAffectorProcessorFromCollider(other);
+		if (affectorProcessor == null)
+			return;
+		if (affectorProcessor.actor == null)
+			return;
+		if (affectorProcessor.actor.team.teamId == (int)Team.eTeamID.DefaultMonster)
+			return;
+
+		ShowIndicator();
+	}
+}
---
+++ Assets/Source/UI/ObjectIndicator/TimeSpaceAltarIndicatorCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSpaceAltarIndicatorCanvas : ObjectIndicatorCanvas
+{
+	public GameObject buttonRootObject;
+	public int positionIndex { get; set; }
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		GetComponent<Canvas>().worldCamera = UIInstanceManager.instance.GetCachedCameraMain();
+	}
+
+	void OnEnable()
+	{
+		InitializeTarget(targetTransform);
+	}
+
+	void OnDisable()
+	{
+		buttonRootObject.SetActive(false);
+	}
+
+	public void OnClickEquipButton()
+	{
+	}
+}

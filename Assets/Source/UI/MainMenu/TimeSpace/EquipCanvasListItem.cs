ef079b486b49fc4f9f05510bca36920563af8cba
hayannal2009 <hayannal2009@gmail.com>
2020-04-03 23:34:51 +0900

    테스트하던 코드 올라가서 되돌린다.

--- Assets/Source/UI/MainMenu/TimeSpace/EquipCanvasListItem.cs
+++ Assets/Source/UI/MainMenu/TimeSpace/EquipCanvasListItem.cs
@@ -32,13 +32,13 @@ public class EquipCanvasListItem : MonoBehaviour
 
 		switch (equipData.cachedEquipTableData.grade)
 		{
-			case 1:
+			case 0:
 				blurImage.color = new Color(0.5f, 0.5f, 0.5f, 0.0f);
 				gradient.color1 = Color.white;
 				gradient.color2 = Color.black;
 				lineColorImage.color = new Color(0.5f, 0.5f, 0.5f);
 				break;
-			case 0:
+			case 1:
 				blurImage.color = new Color(0.28f, 1.0f, 0.78f, 0.0f);
 				gradient.color1 = new Color(0.0f, 1.0f, 0.7f);
 				gradient.color2 = new Color(0.8f, 0.8f, 0.8f);

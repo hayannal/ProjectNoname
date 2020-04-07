c2a4f7c822dbf63b49e0881893a7d14b5fa7e317
hayannal2009 <hayannal2009@gmail.com>
2020-04-07 20:56:11 +0900

    자동장착 및 휴지통 인디케이터 적용

---
+++ Assets/Source/Map/TimeSpaceObject.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 시공간에 있는 서브 오브젝트 두개를 위해 만든 클래스. 일괄장착 표지판 및 일괄판매 휴지통에 쓴다.
+public class TimeSpaceObject : MonoBehaviour
+{
+	public float indicatorDistance = 2.0f;
+	public string indicatorAddress;
+
+	public enum eTimeSpaceObjectType
+	{
+		AutoEquip,
+		Sell,
+	}
+	public eTimeSpaceObjectType timeSpaceObjectType;
+
+	void Start()
+	{
+		_position = transform.position;
+	}
+
+	void OnDisable()
+	{
+		if (_objectIndicatorCanvas != null)
+		{
+			_objectIndicatorCanvas.gameObject.SetActive(false);
+			_objectIndicatorCanvas = null;
+		}
+		_spawnedIndicator = false;
+	}
+
+	Vector3 _position;
+	void Update()
+	{
+		if (_spawnedIndicator == false)
+			return;
+		if (_objectIndicatorCanvas == null)
+			return;
+		if (_objectIndicatorCanvas.gameObject.activeSelf == false)
+			return;
+
+		Vector3 diff = BattleInstanceManager.instance.playerActor.cachedTransform.position - _position;
+		diff.y = 0.0f;
+		if ((diff.x * diff.x + diff.z * diff.z) > indicatorDistance * indicatorDistance)
+		{
+			_objectIndicatorCanvas.gameObject.SetActive(false);
+			_spawnedIndicator = false;
+		}
+	}
+
+	void ShowIndicator()
+	{
+		AddressableAssetLoadManager.GetAddressableGameObject(indicatorAddress, "Canvas", (prefab) =>
+		{
+			if (this == null) return;
+			if (gameObject == null) return;
+			if (gameObject.activeInHierarchy == false) return;
+
+			_objectIndicatorCanvas = (TimeSpaceObjectIndicatorCanvas)UIInstanceManager.instance.GetCachedObjectIndicatorCanvas(prefab);
+			_objectIndicatorCanvas.targetTransform = transform;
+			_objectIndicatorCanvas.SetType(timeSpaceObjectType);
+		});
+
+		_spawnedIndicator = true;
+	}
+
+	TimeSpaceObjectIndicatorCanvas _objectIndicatorCanvas;
+	bool _spawnedIndicator;
+	void OnTriggerEnter(Collider other)
+	{
+		if (_spawnedIndicator)
+			return;
+
+		AffectorProcessor affectorProcessor = BattleInstanceManager.instance.GetAffectorProcessorFromCollider(other);
+		if (affectorProcessor == null)
+			return;
+		if (affectorProcessor.actor == null)
+			return;
+		if (affectorProcessor.actor.team.teamId == (int)Team.eTeamID.DefaultMonster)
+			return;
+
+		ShowIndicator();
+	}
+}
---
+++ Assets/Source/UI/ObjectIndicator/TimeSpaceObjectIndicatorCanvas.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeSpaceObjectIndicatorCanvas : ObjectIndicatorCanvas
+{
+	public GameObject buttonRootObject;
+	public Text[] buttonTextList;
+	public string[] buttonStringIdList;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		GetComponent<Canvas>().worldCamera = UIInstanceManager.instance.GetCachedCameraMain();
+	}
+
+	void OnEnable()
+	{
+		InitializeTarget(targetTransform);
+	}
+
+	void OnDisable()
+	{
+		buttonRootObject.SetActive(false);
+	}
+
+	TimeSpaceObject.eTimeSpaceObjectType _timeSpaceObjectType;
+	public void SetType(TimeSpaceObject.eTimeSpaceObjectType timeSpaceObjectType)
+	{
+		string stringId = "GameUI_AutoEquip";
+		int index = (int)timeSpaceObjectType;
+		if (index < buttonStringIdList.Length)
+			stringId = buttonStringIdList[index];
+		for (int i = 0; i < buttonTextList.Length; ++i)
+			buttonTextList[i].SetLocalizedText(UIString.instance.GetString(stringId));
+
+		_timeSpaceObjectType = timeSpaceObjectType;
+	}
+
+	public void OnClickButton()
+	{
+		switch (_timeSpaceObjectType)
+		{
+			case TimeSpaceObject.eTimeSpaceObjectType.AutoEquip:
+				Debug.Log("Auto Equip!");
+				break;
+			case TimeSpaceObject.eTimeSpaceObjectType.Sell:
+				//UIInstanceManager.instance.ShowCanvasAsync("EquipSellCanvas", null);
+				break;
+		}
+	}
+}

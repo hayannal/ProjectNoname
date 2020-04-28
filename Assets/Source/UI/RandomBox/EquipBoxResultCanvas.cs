ff33171789642cde7b37482552f61785fd8561d2
hayannal2009 <hayannal2009@gmail.com>
2020-04-29 00:24:02 +0900

    안쓰는 using 삭제

--- Assets/Source/UI/RandomBox/EquipBoxResultCanvas.cs
+++ Assets/Source/UI/RandomBox/EquipBoxResultCanvas.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using DG.Tweening;
 using PlayFab.ClientModels;
 
 public class EquipBoxResultCanvas : MonoBehaviour

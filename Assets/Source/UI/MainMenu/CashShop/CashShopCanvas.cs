2f5b805c5cdd10507d49cec841239ccbdf60cdad
hayannal2009 <hayannal2009@gmail.com>
2020-04-29 15:18:03 +0900

    캐릭터박스 굴림 확인창 임시 추가. 슬라이더 적용. 장비박스 다이아 모자를때 처리 추가

--- Assets/Source/UI/MainMenu/CashShop/CashShopCanvas.cs
+++ Assets/Source/UI/MainMenu/CashShop/CashShopCanvas.cs
@@ -42,55 +42,31 @@ public class CashShopCanvas : MonoBehaviour
 		LobbyCanvas.Home();
 	}
 
-
-	DropProcessor _cachedDropProcessor;
-	int _repeatRemainCount;
 	public void OnClickCharacterBox()
 	{
 		// 오리진이나 장비 뽑기와 달리 연속뽑기가 있다.
 		// 첫번째 연출에서는 뽑기상자를 터치해서 열지만 두번째부터는 자동으로 패킷 보내면서 굴려져야한다.
 
 		int characterBoxPrice = 50;
-		_repeatRemainCount = 2;
-		YesNoCanvas.instance.ShowCanvas(true, "confirm", "character box repeat", () =>
+		if (CurrencyData.instance.dia < characterBoxPrice)
 		{
-			// 오리진 박스와 마찬가지로 먼저 드랍프로세서부터 만들어야한다.
-			_cachedDropProcessor = DropProcessor.Drop(BattleInstanceManager.instance.cachedTransform, "Zoflr", "", true, true);
-			PlayFabApiManager.instance.RequestCharacterBox(characterBoxPrice, OnRecvCharacterBox);
-		});
-	}
-
-	void OnRecvCharacterBox(bool serverFailure)
-	{
-		// 실패했는데 굳이 처리해줄 필요가 없다.
-		if (serverFailure)
+			ToastCanvas.instance.ShowToast(UIString.instance.GetString("GameUI_NotEnoughDia"), 2.0f);
 			return;
+		}
 
-		currencySmallInfo.RefreshInfo();
-
-		// 최초 1회는 굴린거니까 1을 차감해둔다.
-		_repeatRemainCount -= 1;
-		
-		// 연출 및 보상 처리.
-		UIInstanceManager.instance.ShowCanvasAsync("RandomBoxScreenCanvas", () =>
-		{
-			// repeatRemainCount를 0으로 보내면 오리진 박스처럼 한번 굴려진 결과가 바로 결과창에 보이게 된다.
-			// 하지만 이 값을 1 이상으로 보내면 내부적으로 n회 돌린 후 누적해서 보여주게 된다.
-			RandomBoxScreenCanvas.instance.SetInfo(_cachedDropProcessor, false, _repeatRemainCount, () =>
-			{
-				// 결과창은 각 패킷이 자신의 Response에 맞춰서 보여줘야한다.
-				// 결과창을 닫을때 RandomBoxScreenCanvas도 같이 닫아주면 알아서 시작점인 CashShopCanvas로 돌아오게 될거다.
-				UIInstanceManager.instance.ShowCanvasAsync("CharacterBoxResultCanvas", () =>
-				{
-					CharacterBoxResultCanvas.instance.RefreshInfo(false);
-				});
-			});
-		});
+		UIInstanceManager.instance.ShowCanvasAsync("CharacterBoxConfirmCanvas", null);
 	}
 
+	DropProcessor _cachedDropProcessor;
 	public void OnClickEquipBox1()
 	{
 		int equipBoxPrice = 30;
+		if (CurrencyData.instance.dia < equipBoxPrice)
+		{
+			ToastCanvas.instance.ShowToast(UIString.instance.GetString("GameUI_NotEnoughDia"), 2.0f);
+			return;
+		}
+
 		YesNoCanvas.instance.ShowCanvas(true, "confirm", "equip box 1", () =>
 		{
 			// 오리진 박스와 마찬가지로 먼저 드랍프로세서부터 만들어야한다.
@@ -102,6 +78,12 @@ public class CashShopCanvas : MonoBehaviour
 	public void OnClickEquipBox8()
 	{
 		int equipBoxPrice = 200;
+		if (CurrencyData.instance.dia < equipBoxPrice)
+		{
+			ToastCanvas.instance.ShowToast(UIString.instance.GetString("GameUI_NotEnoughDia"), 2.0f);
+			return;
+		}
+
 		YesNoCanvas.instance.ShowCanvas(true, "confirm", "equip box 8", () =>
 		{
 			_cachedDropProcessor = DropProcessor.Drop(BattleInstanceManager.instance.cachedTransform, "Wkdwkdql", "", true, true);

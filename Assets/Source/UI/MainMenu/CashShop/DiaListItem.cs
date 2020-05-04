10645b12ec9d52d93e118308366463288b5d8861
hayannal2009 <hayannal2009@gmail.com>
2020-05-04 18:20:54 +0900

    캐시샵 캔버스 작업중

---
+++ Assets/Source/UI/MainMenu/CashShop/DiaListItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiaListItem : MonoBehaviour
+{
+	public Text amountText;
+	public Text priceText;
+
+	public void OnClickButton()
+	{
+
+	}
+}

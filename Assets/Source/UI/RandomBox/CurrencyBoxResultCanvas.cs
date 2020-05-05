dc27fe3d16543d9d22c5d7633aa3af4bcfaf65e8
hayannal2009 <hayannal2009@gmail.com>
2020-05-05 16:29:09 +0900

    재화구매창에선 재화 증가 애니적용

--- Assets/Source/UI/RandomBox/CurrencyBoxResultCanvas.cs
+++ Assets/Source/UI/RandomBox/CurrencyBoxResultCanvas.cs
@@ -17,12 +17,88 @@ public class CurrencyBoxResultCanvas : MonoBehaviour
 		instance = this;
 	}
 
+	void Update()
+	{
+		UpdateGoldText();
+		UpdateDiaText();
+	}
+
+	int _addGold;
+	int _addDia;
 	public void RefreshInfo(int addGold, int addDia)
 	{
+		_addGold = addGold;
+		_addDia = addDia;
 		goldGroupRectTransform.gameObject.SetActive(addGold > 0);
 		diaGroupRectTransform.gameObject.SetActive(addDia > 0);
-		goldValueText.text = addGold.ToString("N0");
-		diaValueText.text = addDia.ToString("N0");
+
+		if (addGold > 0)
+		{
+			goldValueText.text = "0";
+			_goldChangeRemainTime = goldChangeTime;
+			_goldChangeSpeed = _addGold / _goldChangeRemainTime;
+			_currentGold = 0.0f;
+			_updateGoldText = true;
+		}
+
+		if (addDia > 0)
+		{
+			diaValueText.text = "0";
+			_diaChangeRemainTime = diaChangeTime;
+			_diaChangeSpeed = _addDia / _diaChangeRemainTime;
+			_currentDia = 0.0f;
+			_updateDiaText = true;
+		}
+	}
+
+	const float diaChangeTime = 0.6f;
+	float _diaChangeRemainTime;
+	float _diaChangeSpeed;
+	float _currentDia;
+	int _lastDia;
+	bool _updateDiaText;
+	void UpdateDiaText()
+	{
+		if (_updateDiaText == false)
+			return;
+
+		_currentDia += _diaChangeSpeed * Time.deltaTime;
+		int currentDiaInt = (int)_currentDia;
+		if (currentDiaInt >= _addDia)
+		{
+			currentDiaInt = _addDia;
+			_updateDiaText = false;
+		}
+		if (currentDiaInt != _lastDia)
+		{
+			_lastDia = currentDiaInt;
+			diaValueText.text = _lastDia.ToString("N0");
+		}
+	}
+
+	const float goldChangeTime = 0.6f;
+	float _goldChangeRemainTime;
+	float _goldChangeSpeed;
+	float _currentGold;
+	int _lastGold;
+	bool _updateGoldText;
+	void UpdateGoldText()
+	{
+		if (_updateGoldText == false)
+			return;
+
+		_currentGold += _goldChangeSpeed * Time.unscaledDeltaTime;
+		int currentGoldInt = (int)_currentGold;
+		if (currentGoldInt >= _addGold)
+		{
+			currentGoldInt = _addGold;
+			_updateGoldText = false;
+		}
+		if (currentGoldInt != _lastGold)
+		{
+			_lastGold = currentGoldInt;
+			goldValueText.text = _lastGold.ToString("N0");
+		}
 	}
 
 	public void OnClickExitButton()

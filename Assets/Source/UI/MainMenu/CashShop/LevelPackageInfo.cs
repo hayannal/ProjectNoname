1cd20755e8de047b607f3779cbd978baa20dbd2a
hayannal2009 <hayannal2009@gmail.com>
2020-05-06 21:23:20 +0900

    캐시상점 레벨패키지 프리팹으로 분리. 패키지 내용물은 별도 프리팹으로 분리

---
+++ Assets/Source/UI/MainMenu/CashShop/LevelPackageInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelPackageInfo : MonoBehaviour
+{
+	public GameObject[] levelPackagePrefabList;
+
+	public Transform iconImageRootTransform;
+	public Text valueXText;
+	public Text priceText;
+	public RectTransform priceTextTransform;
+	public Text wonText;
+	public Text prevPriceText;
+	public RectTransform lineImageRectTransform;
+	public RectTransform rightTopRectTransform;
+	public Text nameText;
+
+	public void RefreshInfo()
+	{
+
+	}
+
+	public void OnClickButton()
+	{
+
+	}
+}

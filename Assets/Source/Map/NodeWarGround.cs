2436c5e6143b4d030b485f06990c72bcdab3acec
hayannal2009 <hayannal2009@gmail.com>
2020-06-05 03:09:40 +0900

    심리스 Plane 배치 로직 적용. 딜레이 주면서 순차로딩 기능도 적용

--- Assets/Source/Map/NodeWarGround.cs
+++ Assets/Source/Map/NodeWarGround.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class NodeWarGround : MonoBehaviour
 {
 	public static NodeWarGround instance;
-
+	
 	public Canvas worldCanvas;
+	public int planeSize = 40;
 
 	GameObject _planePrefab;
 
@@ -21,30 +22,123 @@ public class NodeWarGround : MonoBehaviour
 		worldCanvas.worldCamera = UIInstanceManager.instance.GetCachedCameraMain();
 	}
 
+	void Update()
+	{
+		UpdateLateInitialize();
+		UpdateSeamlessGround();
+	}
+
+	
+	// 지면은 9개로 나눠서 플레이어 주변에 배치해두기만 하면 된다.
+	List<Collider> _listCurrentPlaneCollider = new List<Collider>();
+
 	// 로딩속도를 위해 만들어지고 Plane 프리팹을 Async로드하는게 아니라 생성될때 함께 로드된 Plane 프리팹을 전달받아서 사용하기로 한다.
-	Dictionary<string, Collider> _dicCurrentPlaneCollider = new Dictionary<string, Collider>();
 	public void InitializeGround(GameObject planePrefab)
 	{
 		_planePrefab = planePrefab;
 
-		// 최초에는 중심에만 하나 만들고 주변 8칸은 한프레임씩 늦게 만들기로 한다.
-		GameObject planeObject = BattleInstanceManager.instance.GetCachedObject(planePrefab, Vector3.zero, Quaternion.identity);
+		// 9개의 슬롯은 미리 만들어둔다.
+		for (int j = -1; j <= 1; ++j)
+		{
+			for (int i = -1; i <= 1; ++i)
+			{
+				_listCurrentPlaneCollider.Add(null);
+			}
+		}
+
+		int middleIndex = 3 * 1 + 2 - 1;
+
+		// 최초에는 중심에만 하나 만들고 주변 8칸은 한프레임씩 늦게 만들기로 한다. 한번에 8개 다 만들면 로딩 느릴까봐 이렇게 해본다.
+		GameObject planeObject = BattleInstanceManager.instance.GetCachedObject(_planePrefab, Vector3.zero, Quaternion.identity);
 		Collider planeCollider = planeObject.GetComponent<Collider>();
-		_dicCurrentPlaneCollider.Add("0x0", planeCollider);
+		_listCurrentPlaneCollider[middleIndex] = planeCollider;
+		_createRemainCount = 8;
+		_invalidChunk = true;
 	}
+
+	bool _skipFrame;
+	int _createRemainCount;
+	void UpdateLateInitialize()
+	{
+		if (_createRemainCount == 0)
+			return;
+		if (_skipFrame)
+		{
+			_skipFrame = false;
+			return;
+		}
+
+		for (int j = 0; j <= 2; ++j)
+		{
+			for (int i = 0; i <= 2; ++i)
+			{
+				int index = j * 3 + i;
+				if (_listCurrentPlaneCollider[index] != null)
+					continue;
+
+				float planePositionX = (_lastChunkX + i - 1) * planeSize;
+				float planePositionZ = (_lastChunkZ + j - 1) * planeSize;
+				GameObject planeObject = BattleInstanceManager.instance.GetCachedObject(_planePrefab, new Vector3(planePositionX, 0.0f, planePositionZ), Quaternion.identity);
+				Collider planeCollider = planeObject.GetComponent<Collider>();
+				_listCurrentPlaneCollider[index] = planeCollider;
+				_createRemainCount -= 1;
+				_skipFrame = true;
+				return;
+			}
+		}
+	}
+
+	bool _invalidChunk = false;
+	int _lastChunkX;
+	int _lastChunkZ;
+	void UpdateSeamlessGround()
+	{
+		Vector3 position = BattleInstanceManager.instance.playerActor.cachedTransform.position;
+
+		// 현재 위치로부터 청크 위치를 계산한 후
+		float calX = (position.x + planeSize * 0.5f) / planeSize;
+		float calZ = (position.z + planeSize * 0.5f) / planeSize;
+		
+		int x = (calX < 0.0f) ? (int)(calX - 1.0f) : (int)calX;
+		int z = (calZ < 0.0f) ? (int)(calZ - 1.0f) : (int)calZ;
+
+		bool refresh = false;
+		if (_lastChunkX != x || _lastChunkZ != z || _invalidChunk)
+			refresh = true;
+
+		if (refresh == false)
+			return;
+
+		//Debug.LogFormat("Refresh Seamless Map. Chunk X : {0} / Chunk Z : {1}", x, z);
+		_lastChunkX = x;
+		_lastChunkZ = z;
+		_invalidChunk = false;
+
+		// 중앙청크 한개와 인근 8개의 청크를 계산해서 월드에 배치한다.
+		for (int j = 0; j <= 2; ++j)
+		{
+			for (int i = 0; i <= 2; ++i)
+			{
+				int index = j * 3 + i;
+				if (_listCurrentPlaneCollider[index] == null)
+					continue;
+				float planePositionX = (_lastChunkX + i - 1) * planeSize;
+				float planePositionZ = (_lastChunkZ + j - 1) * planeSize;
+				_listCurrentPlaneCollider[index].transform.position = new Vector3(planePositionX, 0.0f, planePositionZ);
+			}
+		}
+	}
+
+
+
 
 	public bool CheckPlaneCollider(Collider col)
 	{
-		Dictionary<string, Collider>.Enumerator e = _dicCurrentPlaneCollider.GetEnumerator();
-		while (e.MoveNext())
+		for (int i = 0; i < _listCurrentPlaneCollider.Count; ++i)
 		{
-			if (e.Current.Value == null)
+			if (_listCurrentPlaneCollider[i] == null || _listCurrentPlaneCollider[i].gameObject == null)
 				continue;
-			if (e.Current.Value.gameObject == null)
-				continue;
-			if (e.Current.Value.gameObject.activeSelf == false)
-				continue;
-			if (e.Current.Value != col)
+			if (_listCurrentPlaneCollider[i] != col)
 				continue;
 			return true;
 		}

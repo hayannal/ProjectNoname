0c0f8b0008524fc6aae40436169655d683405df8
hayannal2009 <hayannal2009@gmail.com>
2020-06-14 11:52:54 +0900

    NodeWarItem 획득시 areaEffect 스케일 애니메이션 적용

--- Assets/Source/Map/NodeWarItem.cs
+++ Assets/Source/Map/NodeWarItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class NodeWarItem : MonoBehaviour
 {
@@ -14,6 +15,22 @@ public class NodeWarItem : MonoBehaviour
 	public eItemType itemType;
 	public float getRange = 0.2f;
 
+	public GameObject mainObject;
+	public Transform areaEffectTransform;
+
+	void OnEnable()
+	{
+		if (mainObject != null)
+			mainObject.SetActive(true);
+		if (areaEffectTransform != null)
+			areaEffectTransform.localScale = Vector3.one;
+	}
+
+	void OnDisable()
+	{
+		_waitEndAnimation = false;
+	}
+
 	void Update()
 	{
 		UpdateDistance();
@@ -22,6 +39,9 @@ public class NodeWarItem : MonoBehaviour
 	const float ValidDistance = 30.0f;
 	void UpdateDistance()
 	{
+		if (_waitEndAnimation)
+			return;
+
 		Vector3 playerPosition = BattleInstanceManager.instance.playerActor.cachedTransform.position;
 		float playerRadius = BattleInstanceManager.instance.playerActor.actorRadius;
 		Vector3 position = cachedTransform.position;
@@ -35,20 +55,36 @@ public class NodeWarItem : MonoBehaviour
 			gameObject.SetActive(false);
 	}
 
+	bool _waitEndAnimation;
 	void GetDropObject()
 	{
 		switch (itemType)
 		{
 			case eItemType.Soul:
 				BattleManager.instance.OnGetSoul(cachedTransform.position);
+				_waitEndAnimation = true;
 				break;
 			case eItemType.HealOrb:
 				BattleManager.instance.OnGetHealOrb(cachedTransform.position);
+				_waitEndAnimation = true;
 				break;
 			case eItemType.SpecialPack:
-
+				// 이렇게 먹자마자 사라지는 아이템도 있을 수 있다.
+				//gameObject.SetActive(false);
 				break;
 		}
+
+		if (_waitEndAnimation)
+		{
+			if (mainObject != null)
+				mainObject.SetActive(false);
+			if (areaEffectTransform != null)
+				areaEffectTransform.DOScale(0.0f, 0.25f).SetEase(Ease.OutQuad).OnComplete(OnCompleteAnimation);
+		}
+	}
+
+	void OnCompleteAnimation()
+	{
 		gameObject.SetActive(false);
 	}
 

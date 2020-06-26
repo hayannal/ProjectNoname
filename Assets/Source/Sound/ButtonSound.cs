d0e24aad9fae1b834ebceb804251fc545920b61e
hayannal2009 <hayannal2009@gmail.com>
2020-06-26 21:42:37 +0900

    AddListener로 해야 Back키 눌렀을때 소리도 같이 발생되게 된다.

--- Assets/Source/Sound/ButtonSound.cs
+++ Assets/Source/Sound/ButtonSound.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class ButtonSound : MonoBehaviour, IPointerClickHandler
+public class ButtonSound : MonoBehaviour
 {
 	public string SFXName = "Click";
 
-	public void OnPointerClick(PointerEventData eventData)
+	Button _button;
+	Toggle _toggle;
+
+	void Start()
 	{
+		_button = GetComponent<Button>();
+		if (_button != null)
+		{
+			_button.onClick.AddListener(OnClick);
+		}
+		_toggle = GetComponent<Toggle>();
+		if (_toggle != null)
+		{
+			_toggle.onValueChanged.AddListener(OnToggle);
+		}
+	}
+
+	void OnClick()
+	{
+		PlaySound();
+	}
+
+	void OnToggle(bool toggle)
+	{
+		if (toggle)
+			PlaySound();
+	}
+
+	void PlaySound()
+	{
+		if (_button != null && _button.interactable == false)
+			return;
+		if (_toggle != null && _toggle.interactable == false)
+			return;
+
 		SoundManager.instance.PlaySFX(SFXName);
 	}
 }

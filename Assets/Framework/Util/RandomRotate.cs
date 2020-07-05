e55e2b79d6ad40423b25714b1989b84604c538d9
hayannal2009 <hayannal2009@gmail.com>
2020-07-05 22:52:33 +0900

    RandomRotate 기능 추가. Yuka 공격에 적용해둔다.

---
+++ Assets/Framework/Util/RandomRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotate : MonoBehaviour
+{
+	public bool applyX;
+	public bool applyY;
+	public bool applyZ;
+
+	Transform _transform;
+
+	void Awake()
+	{
+		_transform = GetComponent<Transform>();
+	}
+
+	void OnEnable()
+	{
+		float x = applyX ? Random.Range(0.0f, 360.0f) : 0.0f;
+		float y = applyY ? Random.Range(0.0f, 360.0f) : 0.0f;
+		float z = applyZ ? Random.Range(0.0f, 360.0f) : 0.0f;
+		_transform.Rotate(x, y, z);
+	}
+}

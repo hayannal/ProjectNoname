1cf261e755ae7721f4d38f096c6fe77872cb4f97
hayannal2009 <hayannal2009@gmail.com>
2020-07-09 17:03:41 +0900

    ArcFormGenerator 에 delayedAdditionalCreateCount 기능 적용

--- Assets/Source/HitObject/GeneratorClass/ArcFormGenerator.cs
+++ Assets/Source/HitObject/GeneratorClass/ArcFormGenerator.cs
@@ -7,10 +7,19 @@ public class ArcFormGenerator : ContinuousHitObjectGeneratorBase
 {
 	[Header("ArcFormGenerator")]
 	public float betweenAngle = 15.0f;
+	public int delayedAdditionalCreateCount;
+	public float delay;
 
+	bool _createdInitialArcForm;
+	int _additionalCreateCount;
+	float _remainDelayTime;
 	public override void InitializeGenerator(MeHitObject meHit, Actor parentActor, StatusBase statusBase, int hitSignalIndexInAction, int repeatIndex, int repeatAddCountByLevelPack, Transform spawnTransform)
 	{
 		base.InitializeGenerator(meHit, parentActor, statusBase, hitSignalIndexInAction, repeatIndex, repeatAddCountByLevelPack, spawnTransform);
+
+		_createdInitialArcForm = false;
+		_additionalCreateCount = 0;
+		_remainDelayTime = 0.0f;
 
 		if (_initializedCreateCount == 0)
 			gameObject.SetActive(false);
@@ -25,16 +34,55 @@ public class ArcFormGenerator : ContinuousHitObjectGeneratorBase
 			return;
 		}
 
-		for (int i = 0; i < _initializedCreateCount; ++i)
+		if (_remainDelayTime > 0.0f)
+		{
+			if (_parentActor.actorStatus.IsDie())
+			{
+				gameObject.SetActive(false);
+				return;
+			}
+
+			if (_parentActor.affectorProcessor.IsContinuousAffectorType(eAffectorType.CannotAction))
+				return;
+
+			_remainDelayTime -= Time.deltaTime;
+			if (_remainDelayTime <= 0.0f)
+			{
+				CreateArcForm(_additionalCreateCount, betweenAngle);
+				gameObject.SetActive(false);
+			}
+			return;
+		}
+
+		if (_createdInitialArcForm == false)
+		{
+			CreateArcForm(_initializedCreateCount, betweenAngle);
+			_createdInitialArcForm = true;
+		}
+
+		int count = delayedAdditionalCreateCount;
+		float delay = this.delay;
+		ArcFormHitObjectAffector.GetInfo(_parentActor.affectorProcessor, ref count, ref delay);
+		if (count > 0 && delay > 0.0f)
+		{
+			_additionalCreateCount = count;
+			_remainDelayTime = delay;
+		}
+		else
+			gameObject.SetActive(false);
+	}
+
+	void CreateArcForm(int count, float betweenAngle)
+	{
+		for (int i = 0; i < count; ++i)
 		{
 			float centerAngleY = cachedTransform.eulerAngles.y;
-			float baseAngle = _initializedCreateCount % 2 == 0 ? centerAngleY - (betweenAngle / 2f) : centerAngleY;
+			float baseAngle = count % 2 == 0 ? centerAngleY - (betweenAngle / 2f) : centerAngleY;
 			float angle = WavingNwayGenerator.GetShiftedAngle(i, baseAngle, betweenAngle);
 
 			HitObject hitObject = Generate(cachedTransform.position, Quaternion.Euler(0.0f, angle, 0.0f), true);
 			if (hitObject != null)
 				hitObject.cachedTransform.Translate(0.0f, 0.0f, 0.5f, Space.Self);
 		}
-		gameObject.SetActive(false);
 	}
 }

6abb27ee8f03996248d5cda477a39b3ae11b534a
hayannal2009 <hayannal2009@gmail.com>
2020-07-10 19:46:31 +0900

    제일 기본인 TransformForward 제네레이터 추가.

---
+++ Assets/Source/HitObject/GeneratorClass/TransformForwardGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ActorStatusDefine;
+
+public class TransformForwardGenerator : ContinuousHitObjectGeneratorBase
+{
+	[Header("TransformForwardGenerator")]
+	public float interval;
+
+	int _remainCreateCount;
+	float _remainIntervalTime;
+
+	public override void InitializeGenerator(MeHitObject meHit, Actor parentActor, StatusBase statusBase, int hitSignalIndexInAction, int repeatIndex, int repeatAddCountByLevelPack, Transform spawnTransform)
+	{
+		base.InitializeGenerator(meHit, parentActor, statusBase, hitSignalIndexInAction, repeatIndex, repeatAddCountByLevelPack, spawnTransform);
+
+		_remainCreateCount = _initializedCreateCount;
+		_remainIntervalTime = 0.0f;
+
+		if (_remainCreateCount == 0)
+			gameObject.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (CheckChangeState())
+		{
+			gameObject.SetActive(false);
+			return;
+		}
+
+		if (_parentActor.actorStatus.IsDie())
+		{
+			gameObject.SetActive(false);
+			return;
+		}
+
+		if (_parentActor.affectorProcessor.IsContinuousAffectorType(eAffectorType.CannotAction))
+			return;
+
+		_remainIntervalTime -= Time.deltaTime;
+		if (_remainIntervalTime < 0.0f)
+		{
+			_remainCreateCount -= 1;
+			_remainIntervalTime += interval;
+
+			Generate(cachedTransform.position, cachedTransform.rotation);
+		}
+
+		if (_remainCreateCount <= 0)
+			gameObject.SetActive(false);
+	}
+}

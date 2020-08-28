e3184d3e91e8206050e278561df6e0e743ec34c5
hayannal2009 <hayannal2009@gmail.com>
2020-08-28 11:16:24 +0900

    빅뱃 못뽑게 하는 처리에 버그가 있어서 수정

--- Assets/Source/Drop/DropManager.cs
+++ Assets/Source/Drop/DropManager.cs
@@ -582,7 +582,10 @@ public class DropManager : MonoBehaviour
 		}
 
 		CharacterData characterData = PlayerData.instance.GetCharacterData(actorId);
-		if (characterData == null && actorId != "Actor2103")
+		if (characterData == null && actorId == "Actor2103")
+			return false;
+
+		if (characterData == null)
 			return true;
 
 		if (characterData.needLimitBreak && characterData.limitBreakPoint <= characterData.limitBreakLevel)

985f3f5a44486b1b1ace51e0fae591b626253b36
hayannal2009 <hayannal2009@gmail.com>
2020-11-10 20:34:25 +0900

    영수증 처리를 위해 Framework 폴더에 IAP 폴더 추가하고 영수증 파싱 위한 파일들 추가.

---
+++ Assets/Framework/IAP/iOSPurchaseData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct iOSReceiptData
+{
+	public string Store;
+	public string TransactionID;
+	public string Payload;
+
+	public iOSReceiptData(string json)
+	{
+		iOSReceiptData data = JsonUtility.FromJson<iOSReceiptData>(json);
+		this = data;
+	}
+}

96ecec0e31dc4fd64de1a15e3dcde349e11b7701
hayannal2009 <hayannal2009@gmail.com>
2020-11-12 01:41:33 +0900

    Revert "IAP 2.2.0으로 업데이트" -> 괜히 2.2.0 해서 Cancel도 안오고 오류만 난다. 롤백
    This reverts commit 5f792b7c2ffa27297530ecc04e37cda66484edf4.

--- Assets/Plugins/UnityPurchasing/script/IAPButton.cs
+++ Assets/Plugins/UnityPurchasing/script/IAPButton.cs
@@ -134,12 +134,6 @@ namespace UnityEngine.Purchasing
                     CodelessIAPStoreListener.Instance.ExtensionProvider.GetExtension<ISamsungAppsExtensions>()
                         .RestoreTransactions(OnTransactionsRestored);
                 }
-                else if (Application.platform == RuntimePlatform.Android &&
-                    StandardPurchasingModule.Instance().appStore == AppStore.GooglePlay)
-                {
-                    CodelessIAPStoreListener.Instance.ExtensionProvider.GetExtension<IGooglePlayStoreExtensions>()
-                        .RestoreTransactions(OnTransactionsRestored);
-                }
                 else
                 {
                     Debug.LogWarning(Application.platform.ToString() +

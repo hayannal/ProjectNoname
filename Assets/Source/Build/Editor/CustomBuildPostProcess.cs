99e111b33d31c0d8a21a2d5e974b7c2f57f993af
hayannal2009 <hayannal2009@gmail.com>
2020-11-26 23:56:39 +0900

    iOS에서 리젝되면 이런게 필요할수도 있을거 같다. 우선은 처리하지 않고 올려보긴 한다.

---
+++ Assets/Source/Build/Editor/CustomBuildPostProcess.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public static class CustomBuildPostProcess
+{
+	[PostProcessBuild(999)]
+	public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+	{
+		/*
+		if (buildTarget == BuildTarget.iOS)
+		{
+			// Bitcode
+			{
+				string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+				PBXProject pbxProject = new PBXProject();
+				pbxProject.ReadFromFile(projectPath);
+
+				string target = pbxProject.TargetGuidByName("Unity-iPhone");
+				pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+
+				pbxProject.WriteToFile(projectPath);
+			}
+
+			// NonExemptEncryption
+			{
+				string infoPlistPath = path + "/Info.plist";
+
+				PlistDocument plistDoc = new PlistDocument();
+				plistDoc.ReadFromFile(infoPlistPath);
+				if (plistDoc.root != null)
+				{
+					plistDoc.root.SetBoolean("ITSAppUsesNonExemptEncryption", false);
+					//plistDoc.root.SetString("CFBundleDisplayName", "MY APP NAME");
+					plistDoc.WriteToFile(infoPlistPath);
+				}
+				else
+				{
+					Debug.LogError("ERROR: Can't open " + infoPlistPath);
+				}
+			}
+		}
+		*/
+	}
+}

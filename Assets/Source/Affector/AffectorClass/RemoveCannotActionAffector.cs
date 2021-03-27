35d37f76dacb3e5506af7f0b7112123b361dae3d
hayannal2009 <hayannal2009@gmail.com>
2021-03-27 18:43:28 +0900

    이펙트 로직 수정 sValue2 추가.

--- Assets/Source/Affector/AffectorClass/RemoveCannotActionAffector.cs
+++ Assets/Source/Affector/AffectorClass/RemoveCannotActionAffector.cs
@@ -5,6 +5,8 @@ using DG.Tweening;
 public class RemoveCannotActionAffector : AffectorBase
 {
 	float _endTime;
+	GameObject _startAttachEffectPrefab;
+	Transform _startAttachEffectTransform;
 	GameObject _startEffectPrefab;
 	Transform _startEffectTransform;
 	Transform _loopEffectTransform;
@@ -33,9 +35,13 @@ public class RemoveCannotActionAffector : AffectorBase
 
 		if (!string.IsNullOrEmpty(affectorValueLevelTableData.sValue4))
 			_startEffectPrefab = FindPreloadObject(affectorValueLevelTableData.sValue4);
-
 		if (_startEffectPrefab != null)
 			_startEffectTransform = BattleInstanceManager.instance.GetCachedObject(_startEffectPrefab, _actor.cachedTransform.position, Quaternion.identity).transform;
+
+		if (!string.IsNullOrEmpty(affectorValueLevelTableData.sValue2))
+			_startAttachEffectPrefab = FindPreloadObject(affectorValueLevelTableData.sValue2);
+		if (_startAttachEffectPrefab != null)
+			_startAttachEffectTransform = BattleInstanceManager.instance.GetCachedObject(_startAttachEffectPrefab, _actor.cachedTransform.position, Quaternion.identity).transform;
 	}
 
 	public override void OverrideAffector(AffectorValueLevelTableData affectorValueLevelTableData, HitParameter hitParameter)
@@ -44,13 +50,20 @@ public class RemoveCannotActionAffector : AffectorBase
 
 		if (_startEffectTransform != null)
 			_startEffectTransform.gameObject.SetActive(false);
-
 		if (_startEffectPrefab != null)
 			_startEffectTransform = BattleInstanceManager.instance.GetCachedObject(_startEffectPrefab, _actor.cachedTransform.position, Quaternion.identity).transform;
+
+		if (_startAttachEffectTransform != null)
+			_startAttachEffectTransform.gameObject.SetActive(false);
+		if (_startAttachEffectPrefab != null)
+			_startAttachEffectTransform = BattleInstanceManager.instance.GetCachedObject(_startAttachEffectPrefab, _actor.cachedTransform.position, _actor.cachedTransform.rotation, _actor.cachedTransform).transform;
 	}
 
 	public override void UpdateAffector()
 	{
+		if (CheckEndTime(_endTime) == false)
+			return;
+
 		if (_actor.actorStatus.IsDie())
 		{
 			finalized = true;

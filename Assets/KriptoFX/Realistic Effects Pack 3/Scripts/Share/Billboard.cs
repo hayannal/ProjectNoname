7c6eecf5a2476ac2a29cbc1274994f488f69e69e
hayannal2009 <hayannal2009@gmail.com>
2021-04-04 20:24:53 +0900

    적어둔 각도를 기억하는 customBillboard 옵션 추가.
    MobileFemale이 쓰는 FrozenMine_D 에 적용하려고 추가하는거다.

--- Assets/KriptoFX/Realistic Effects Pack 3/Scripts/Share/Billboard.cs
+++ Assets/KriptoFX/Realistic Effects Pack 3/Scripts/Share/Billboard.cs
@@ -6,12 +6,21 @@ public class Billboard : MonoBehaviour
   public Camera Camera;
   public bool Active = true;
   public bool AutoInitCamera = true;
+  public bool customBillboard = false;
 
   private GameObject myContainer;
   private Transform t, camT, contT;
+  private Vector3 defaultEuler;
 
   private void Awake()
   {
+	if (customBillboard)
+	{
+		defaultEuler = transform.localEulerAngles;
+		Active = false;
+		return;
+	}
+
     if (AutoInitCamera) {
       Camera = Camera.main;
       Active = true;
@@ -27,7 +36,13 @@ public class Billboard : MonoBehaviour
     contT.parent = parent;
   }
 
-  private void Update()
+	private void OnEnable()
+	{
+		if (customBillboard)
+			transform.rotation = Quaternion.Euler(defaultEuler);
+	}
+
+	private void Update()
   {
     if (Active)
       contT.LookAt(contT.position + camT.rotation * Vector3.back, camT.rotation * Vector3.up);

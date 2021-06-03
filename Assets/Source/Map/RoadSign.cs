10a0ed9ab335c6f804f452ab2311d19efcd047f6
hayannal2009 <hayannal2009@gmail.com>
2021-06-04 02:21:12 +0900

    0-1 재진입때 메세지 잘못 처리되던거 수정

--- Assets/Source/Map/RoadSign.cs
+++ Assets/Source/Map/RoadSign.cs
@@ -8,9 +8,17 @@ public class RoadSign : MonoBehaviour
 {
 	public string uiStringKey;
 
+	bool _spawnedGatePillar = false;
 	void OnEnable()
 	{
 		_indicatorShowRemainTime = 0.4f;
+
+		// ClientSaveData에 새로운 타입을 추가해서 저장할까 하다가 게이트필라가 보여진 상태라면 동작하지 않으면 되는거라 게이트필라 여부를 가져다쓰기로 한다.
+		if (ClientSaveData.instance.IsLoadingInProgressGame())
+		{
+			if (ClientSaveData.instance.GetCachedGatePillar())
+				_spawnedGatePillar = true;
+		}
 	}
 
 	void OnDisable()
@@ -50,7 +58,7 @@ public class RoadSign : MonoBehaviour
 			_objectIndicatorCanvas.targetTransform = transform;
 
 			// 어차피 하나만 보일거라서 이렇게 instance로도 접근 가능.
-			MultipleLineIndicatorCanvas.instance.contextText.SetLocalizedText(UIString.instance.GetString(uiStringKey));
+			MultipleLineIndicatorCanvas.instance.contextText.SetLocalizedText(UIString.instance.GetString(_spawnedGatePillar ? "Tutorial_GatePillarMapClear" : uiStringKey));
 		});
 	}
 }

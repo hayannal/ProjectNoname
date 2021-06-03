76ba28a43ac957c33a5989be3b6de1462fa2d59d
hayannal2009 <hayannal2009@gmail.com>
2021-06-03 19:56:09 +0900

    튜토리얼 1스테이지 안내기능 추가.

---
+++ Assets/Source/Map/ClearStageSubTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearStageSubTrigger : MonoBehaviour
+{
+	void OnEnable()
+	{
+		_checkedTrigger = false;
+
+		// ClearStageTrigger 구조에서 변형해서 처리한다.
+		if (ClientSaveData.instance.IsLoadingInProgressGame())
+		{
+			if (ClientSaveData.instance.GetCachedGatePillar())
+				_checkedTrigger = true;
+		}
+
+		if (_checkedTrigger)
+			return;
+
+		UIInstanceManager.instance.ShowCanvasAsync("DragGuideCanvas", () =>
+		{
+			_dragGuideCanvasObject = DragGuideCanvas.instance.gameObject;
+		}, false);
+	}
+
+	GameObject _dragGuideCanvasObject;
+	bool _checkedTrigger;
+	void OnTriggerEnter(Collider other)
+	{
+		if (_checkedTrigger)
+			return;
+
+		AffectorProcessor affectorProcessor = BattleInstanceManager.instance.GetAffectorProcessorFromCollider(other);
+		if (affectorProcessor == null)
+			return;
+		if (affectorProcessor.actor == null)
+			return;
+		if (affectorProcessor.actor.team.teamId == (int)Team.eTeamID.DefaultMonster)
+			return;
+
+		if (_dragGuideCanvasObject != null)
+			_dragGuideCanvasObject.SetActive(false);
+
+		_checkedTrigger = true;
+	}
+}
---
+++ Assets/Source/UI/Lobby/DragGuideCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragGuideCanvas : MonoBehaviour
+{
+	public static DragGuideCanvas instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	// 인풋 감지 하려다가 SubTrigger에 도착하는거로 끄기로 해서 우선 패스
+	//void Update()
+	//{
+	//}
+}

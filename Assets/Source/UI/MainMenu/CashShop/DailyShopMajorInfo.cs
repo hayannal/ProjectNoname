7b31e5edd798f472e24366cd51c1cf29497dc0ca
hayannal2009 <hayannal2009@gmail.com>
2021-06-14 21:37:31 +0900

    데이 표기 수정

--- Assets/Source/UI/MainMenu/CashShop/DailyShopMajorInfo.cs
+++ Assets/Source/UI/MainMenu/CashShop/DailyShopMajorInfo.cs
@@ -74,7 +74,10 @@ public class DailyShopMajorInfo : MonoBehaviour
 			TimeSpan remainTime = _dailyResetTime - ServerTime.UtcNow;
 			if (_lastRemainTimeSecond != (int)remainTime.TotalSeconds)
 			{
-				remainTimeText.text = string.Format("{0:00}:{1:00}:{2:00}", remainTime.Hours, remainTime.Minutes, remainTime.Seconds);
+				if (remainTime.Days > 0)
+					remainTimeText.text = string.Format("{0}d {1:00}:{2:00}:{3:00}", remainTime.Days, remainTime.Hours, remainTime.Minutes, remainTime.Seconds);
+				else
+					remainTimeText.text = string.Format("{0:00}:{1:00}:{2:00}", remainTime.Hours, remainTime.Minutes, remainTime.Seconds);
 				_lastRemainTimeSecond = (int)remainTime.TotalSeconds;
 			}
 		}
